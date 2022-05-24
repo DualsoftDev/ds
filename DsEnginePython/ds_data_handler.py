394b0becd46cf70e1e5a8873dfb2615cd2ba0301
= <=>
2022-05-24 11:50:30 +0900

    update kafka handling & remove remote relays

--- DsEnginePython/ds_data_handler.py
+++ DsEnginePython/ds_data_handler.py
@@ -3,15 +3,16 @@ import time
 import copy
 import ctypes 
 import threading
+import traceback
 from typing import List, Dict
 from datetime import datetime
 from observer_threading import IObserver
 from observer_threading import Observable
-from ds_signal_handler import ds_causal, ds_status, ds_object, ds_system_flag
+from ds_signal_handler import ds_status, ds_object, ds_system_flag
 from ds_signal_handler import signal_status, signal_set
-from ds_signal_handler import get_start_signals, get_end_signals
-from ds_signal_handler import get_reset_signals, get_clear_signals
+from ds_signal_handler import compare_signals
 from ds_signal_handler import get_origin_status, get_type
+from ds_signal_handler import calc_now_status
 from functools import reduce
 from ds_engine_producer import send_data
 
@@ -25,226 +26,168 @@ class ds_signal_exchanger(IObserver):
         self.name:str = None
         self.imparter:Dict[str, imparter] = {}
         self.in_signals:Dict[str, signal_set] = {}
-        self.start_signals:List[str] = []
-        self.reset_signals:List[str] = []
-        self.end_signals:List[str] = []
-        self.clear_signals:List[str] = []
-        self.homing_signals:List[str] = []
+        self.start_signals:Dict[str, ds_status] = {}
+        self.start_expression:str = None
+        self.reset_signals:Dict[str, ds_status] = {}
+        self.reset_expression:str = None
+        self.end_signals:Dict[str, ds_status] = {}
+        self.clear_signals:Dict[str, signal_set] = {}
+        self.homing_signals:Dict[str, signal_set] = {}
         self.origin_signals:List[str] = []
         self.start_points:List[int] = []
         self.signal_onoff = signal_set()
-        self.causal_type = ds_causal.G
         self.status = ds_status.R
         self.event = threading.Event()
         self.thread_id = None
-
-    def calc_now_status(self, _signal:signal_set):
-        if _signal == signal_set(False, False, False):
-            self.status = ds_status.R
-        elif _signal.start == True and not _signal.end == True:
-            my_type = get_type(self.name)
-            if my_type == ds_object.Remote:
-                tag_name = self.name.replace("Remote", "Tag")
-                if tag_name in self.in_signals and\
-                    self.in_signals[tag_name].start == True:
-                    self.status = ds_status.G
-            else:
-                self.status = ds_status.G
-        elif _signal.end == True and _signal.reset == False:
-            self.status = ds_status.F
-        elif _signal.reset == True and _signal.end == True:
-            self.status = ds_status.H
-
-        return self.status
+        self.consumer_group = -1
 
     def truncate_signals(self, _new_signal:signal_set, _signals):
         for signal in _signals:
-            if self.calc_now_status(_new_signal) == ds_status.G:
+            if calc_now_status(_new_signal) == ds_status.G:
                 self.in_signals[signal].end = False
                 self.in_signals[signal].start = False
-            elif self.calc_now_status(_new_signal) == ds_status.H:
+            elif calc_now_status(_new_signal) == ds_status.H:
                 self.in_signals[signal].start = False
                 self.in_signals[signal].reset = False
 
-    def get_target_signals(self, _my_type, _target_signals, _signal_func):
+    def get_target_signals(self, _target_signals, _signal_func):
         return [
-            _signal_func((self.name, _my_type, name, self.in_signals[name]))
+            _signal_func(
+                (
+                    self.in_signals[name],
+                    _target_signals[name] if type(_target_signals) == dict
+                    else None
+                )
+            )
             for name in _target_signals
             if name in self.in_signals
         ]
 
-    def origin_checker(self, _my_type):
-        if len(self.origin_signals) > 0:
-            # print(
-            #   f"now status ---- "
-            #   f"{self.get_target_signals(_my_type, self.origin_signals, get_origin_status)}"
-            # )
-            theta = reduce(
-                lambda num, bin: (num<<1) + int(bin), \
-                self.get_target_signals(_my_type, self.origin_signals, get_origin_status)
-            )
-            # print(f"{self.name} : ----------- theta : {theta}")
-            if not theta in self.start_points:
-                return False
-        return True
+    def origin_checker(self):
+        try:
+            if len(self.origin_signals) > 0:
+                # print(
+                #     f"now status ---- "
+                #     f"{self.get_target_signals(self.origin_signals, get_origin_status)}"
+                # )
+                theta = reduce(
+                    lambda num, bin: (num<<1) + int(bin), \
+                    self.get_target_signals(self.origin_signals, get_origin_status)
+                )
+                # print(f"{self.name} : ----------- theta : {theta}")
+                if not theta in self.start_points:
+                    return False
+            return True
+        except Exception as ex:
+            print(self.name, "has origin error", ex)
+            print(traceback.format_exc())
+            pass
+
+    def check_ready_to_going(self, _my_type:ds_object, _new_onoff_signal:signal_set):
+        try:
+            res = False
+            res = self.get_target_signals(self.start_signals, compare_signals)
+
+            # RELAY : all
+            # TAG & PORT : any
+            start_checker = False
+            now_type = get_type(self.name)
+            basic_condition = len(res) > 0 and self.system_flags.always_on
+            if now_type == ds_object.Relay:
+                start_checker = basic_condition and all(res)
+            elif now_type == ds_object.Tag:
+                start_checker = basic_condition and any(res)
+            else:
+                start_checker = \
+                    basic_condition and any(res) and self.origin_checker()
+
+            if start_checker == True:
+                _new_onoff_signal.start = True
+
+                # need condition of macro for push type
+                self.truncate_signals(_new_onoff_signal, self.start_signals)
+            
+            return _new_onoff_signal
+        except Exception as ex:
+            print(self.name, "has start error", ex)
+            print(traceback.format_exc())
+            pass
+
+    def check_going_to_finish(self, _my_type:ds_object, _new_onoff_signal:signal_set):
+        try:
+            res = self.get_target_signals(self.end_signals, compare_signals)
+            if len(res) > 0 and self.system_flags.always_on and all(res):
+                _new_onoff_signal.end = True
+                _new_onoff_signal.start = False
+                # print(f"{self.name} -- going to finish : {res} in {self.in_signals}")
+            
+            # print(self.thread_id, self.name, _new_onoff_signal)
+            return _new_onoff_signal
+        except Exception as ex:
+            print(self.name, "has end error", ex)
+            print(traceback.format_exc())
+            pass
+    
+    def check_finish_to_homing(self, _my_type:ds_object, _new_onoff_signal:signal_set):
+        try:
+            res = self.get_target_signals(self.reset_signals, compare_signals)
+            basic_condition = len(res) > 0 and self.system_flags.always_on
+
+            if basic_condition and any(res):
+                _new_onoff_signal.reset = True
+                _new_onoff_signal.start = False
+
+                # need condition of macro for push type
+                self.truncate_signals(_new_onoff_signal, self.reset_signals)
+
+            return _new_onoff_signal
+        except Exception as ex:
+            print(self.name, "has reset error", ex)
+            print(traceback.format_exc())
+            pass
+
+    def check_homing_to_ready(self, _my_type:ds_object, _new_onoff_signal:signal_set):
+        try:
+            res = self.get_target_signals(self.clear_signals, compare_signals)
+            basic_condition = len(res) > 0 and self.system_flags.always_on
+            if _my_type == ds_object.Segment:
+                homing_res = True
+                if len(self.homing_signals) > 0:
+                    homing_res = \
+                        self.get_target_signals(self.homing_signals, compare_signals)
+                if basic_condition and all(res) \
+                    and self.origin_checker() and homing_res:
+                    _new_onoff_signal = signal_set()
+            else:
+                if basic_condition and all(res):
+                    _new_onoff_signal = signal_set()
+            
+            return _new_onoff_signal
+        except Exception as ex:
+            print(self.name, "has clear error", ex)
+            print(traceback.format_exc())
+            pass
 
     def estimate(self):
         my_type = get_type(self.name)
         new_onoff_signal = copy.deepcopy(self.signal_onoff)
-        if new_onoff_signal == signal_set(): # Ready
-            try:
-                res = False
-                
-                if self.causal_type == ds_causal.G:
-                    res = self.get_target_signals(my_type, self.start_signals, get_start_signals)
-                else:
-                    res = self.get_target_signals(my_type, self.reset_signals, get_reset_signals)
-                    # print(self.name, "=================", res, self.reset_signals)
+        now_status = calc_now_status(new_onoff_signal)
 
-                # RELAY : all
-                # TAG & PORT : any
-                start_checker = False
-                now_type = get_type(self.name)
-                if now_type == ds_object.Relay:
-                    if self.causal_type == ds_causal.G:
-                        start_checker = len(res) > 0 and all(res) and self.system_flags.always_on
-                    else:
-                        finished = False
-                        tag_name = copy.deepcopy(self.name)
-                        tag_name = tag_name.replace("Relay", "Tag")
-                        if tag_name in self.in_signals and\
-                            self.in_signals[tag_name].finish == True:
-                            finished = True
-                        finish_checker = self.system_flags.always_on and finished
+        if now_status == ds_status.R:
+            return self.check_ready_to_going(my_type, new_onoff_signal)
+        elif now_status == ds_status.G:
+            return self.check_going_to_finish(my_type, new_onoff_signal)
+        elif now_status == ds_status.F:
+            return self.check_finish_to_homing(my_type, new_onoff_signal)
+        elif now_status == ds_status.H:
+            return self.check_homing_to_ready(my_type, new_onoff_signal)
 
-                elif now_type == ds_object.Remote:
-                    going_checker = False
-                    tag_name = copy.deepcopy(self.name)
-                    tag_name = tag_name.replace("Remote", "Tag")
-                    if tag_name in self.in_signals and\
-                        self.in_signals[tag_name].start == True:
-                        going_checker = True
-                    start_checker = self.system_flags.always_on and going_checker
-                elif now_type == ds_object.Segment:
-                    start_checker = \
-                        len(res) > 0 and any(res) and\
-                        self.origin_checker(my_type) and self.system_flags.always_on
-                else:
-                    start_checker = len(res) > 0 and any(res) and self.system_flags.always_on
-
-                if self.causal_type == ds_causal.G:
-                    if start_checker == True:
-                        new_onoff_signal.start = True
-
-                        # need condition of macro for push type
-                        self.truncate_signals(new_onoff_signal, self.start_signals)
-                else:
-                    if finish_checker == True:
-                        new_onoff_signal.end = True
-                        new_onoff_signal.start = False
-                    elif len(res) > 0 and all(res) and self.system_flags.always_on:
-                        new_onoff_signal.start = True
-            except Exception as ex:
-                # print(self.name, "has start error", ex)
-                pass
-        elif new_onoff_signal.start == True and new_onoff_signal.end == False: # Going
-            try:
-                res = self.get_target_signals(my_type, self.end_signals, get_end_signals)
-                if len(res) > 0 and all(res) and self.system_flags.always_on:
-                    new_onoff_signal.end = True
-                    new_onoff_signal.start = False
-            except Exception as ex:
-                # print(self.name, "has end error", ex)
-                pass
-        elif new_onoff_signal.reset == False and new_onoff_signal.end == True: # Finish
-            try:
-                if self.causal_type == ds_causal.G:
-                    res = self.get_target_signals(my_type, self.reset_signals, get_reset_signals)
-                    if len(res) > 0 and any(res) and self.system_flags.always_on:
-                        new_onoff_signal.reset = True
-                        new_onoff_signal.start = False
-
-                    # For force clear by a remote tag when my type is a tag
-                    if my_type == ds_object.Tag:
-                        res = self.get_target_signals(my_type, self.clear_signals, get_clear_signals)
-                        if len(res) > 0 and all(res) and self.system_flags.always_on:
-                            new_onoff_signal = signal_set()
-
-                        # need condition of macro for push type
-                        # self.truncate_signals(new_onoff_signal, self.reset_signals)
-                else:
-                    res = self.get_target_signals(my_type, self.clear_signals, get_clear_signals)
-                    if len(res) > 0 and all(res) and self.system_flags.always_on:
-                        new_onoff_signal = signal_set()
-            except Exception as ex:
-                # print(self.name, "has reset error", ex)
-                pass
-        elif new_onoff_signal.reset == True: # Homing
-            try:
-                res = self.get_target_signals(my_type, self.clear_signals, get_clear_signals)
-                if len(res) > 0 and all(res) and self.system_flags.always_on:
-                    if my_type == ds_object.Segment:
-                        homing_res = True
-                        if len(self.homing_signals) > 0:
-                            homing_res = \
-                                self.get_target_signals(
-                                    my_type, self.homing_signals, get_end_signals
-                                )
-                        if self.origin_checker(my_type) and homing_res:
-                            new_onoff_signal = signal_set()
-                    else:
-                        new_onoff_signal = signal_set()
-            except Exception as ex:
-                # print(self.name, "has clear error", ex)
-                pass
-
-        return new_onoff_signal
-
-    def estimate_new_status(self, _onoff_status:signal_status):
+    def update(self, _id, _onoff_status:signal_status):
+        self.in_signals[_onoff_status.name] = copy.deepcopy(_onoff_status.signal)
         estimated = self.estimate()
         if not self.signal_onoff == estimated:
             self.signal_onoff = copy.deepcopy(estimated)
             self.event.set()
-
-            if len(self.clear_signals) == 0:
-                if self.signal_onoff.start == True and\
-                    self.signal_onoff.end == False and\
-                    get_type(self.name) == ds_object.Segment:
-                    if len(self.end_signals) > 0:
-                        estimated = self.estimate()
-                        if not self.signal_onoff == estimated:
-                            time.sleep(1.0)
-                            self.signal_onoff.end = True
-                            self.signal_onoff.start = False
-                            self.event.set()
-                            return
-                    else:
-                        time.sleep(1.0)
-                        self.signal_onoff.end = True
-                        self.signal_onoff.start = False
-                        self.event.set()
-                        return
-                elif self.signal_onoff.end == True and\
-                    self.signal_onoff.reset == True:
-                    if len(self.clear_signals) == 0:
-                        time.sleep(0.5)
-                        self.signal_onoff = signal_set()
-                        self.event.set()
-                        return
-
-    def update(self, _id, _onoff_status:signal_status):
-        self.in_signals[_onoff_status.name] = copy.deepcopy(_onoff_status.signal)
-
-        if not self.name == _onoff_status.name:
-            self.estimate_new_status(_onoff_status)
-        
-        if get_type(self.name) == ds_object.Relay and\
-            self.calc_now_status(self.signal_onoff) == ds_status.R or\
-            self.calc_now_status(self.signal_onoff) == ds_status.F:
-            estimated = self.estimate()
-            if not self.signal_onoff == estimated:
-                self.signal_onoff = copy.deepcopy(estimated)
-                self.event.set()
 
     def connect(self, _name:str, _parent:str, _imparter:imparter):
         self.name = _name
@@ -259,17 +202,19 @@ class ds_signal_exchanger(IObserver):
         t = time.time()
         print(
             f"{self.name}[{_id}] : "\
-            f"{self.calc_now_status(self.signal_onoff)}"\
+            f"{calc_now_status(self.signal_onoff)}"\
             f" - {datetime.fromtimestamp(t)}"
         )
         send_data(
             {
                 "name" : f"{self.name}",
                 "signal" : dumps(self.signal_onoff.__dict__),
-                "status" : f"{self.calc_now_status(self.signal_onoff)}",
-                "time" : f"{datetime.fromtimestamp(t)}"
+                "status" : f"{calc_now_status(self.signal_onoff)}",
+                "time" : f"{datetime.fromtimestamp(t)}",
+                "group_id" : self.consumer_group
             }
         )
+        time.sleep(0.25)
         for parent in self.imparter:
             self.imparter[parent].notify_status(
                 _id, 
@@ -292,13 +237,15 @@ class thread_with_exception(threading.Thread):
                 if self.client.thread_id == None:
                     self.client.thread_id = self.get_id()
                 self.client.event.wait()
-                self.client.change_event(self.client.thread_id)
                 self.client.event.clear()
+                self.client.change_event(self.client.thread_id)
         except Exception as ex:
             print(
                 "error message : ", self.client.name, \
-                " - ", ex, " - ", self.client.in_signals
+                " - ", ex, " - ", self.client.in_signals, \
+                "thread id : ", self.client.thread_id
             )
+            print(traceback.format_exc())
         finally: 
             print("ended") 
            

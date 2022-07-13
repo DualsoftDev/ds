80947633c0bcecd22c53776daa54309d275b72e1
= <=>
2022-07-13 20:08:30 +0900

    Fix finding call segment reset source

--- DsDotNet/src/Engine.Graph/Graph.fs
+++ DsDotNet/src/Engine.Graph/Graph.fs
@@ -143,8 +143,22 @@ module GraphUtil =
         (edges2QgEdge e) |> GraphExtensions.ToAdjacencyGraph
 
     let checkSourceInGraph(sourceFlow:Flow) (src:IVertex) = 
-        GraphInfo(seq[sourceFlow]).SolidConnectedComponets
-        |> Seq.filter(fun gs -> gs |> Seq.contains(src))
+        let gri = GraphInfo(seq[sourceFlow])
+        let routes = 
+            getInits(gri.SolidGraph)
+            |> Seq.map(fun s ->
+                (computeDijkstra gri.SolidGraph s src)
+                |> Seq.collect(fun e -> [e.Source; e.Target])
+                |> Seq.distinct
+            )
+            |> Seq.filter(fun r -> (r |> Seq.length) <> 0)
+            |> Seq.collect id
+        let graphs = 
+            gri.SolidConnectedComponets
+            |> Seq.filter(fun gs -> gs |> Seq.contains(src))
+            |> Seq.collect id
+
+        routes, graphs
 
     let findGraphIncludeSource(rSrc:IVertex) =
         match rSrc with
@@ -158,31 +172,23 @@ module GraphUtil =
     let targetResetMarker(srcs:ITxRx seq) (tgts:ITxRx seq) =
         let sourceSegs = srcs |> Seq.map(fun v -> v:?>IVertex)
         let targetSegs = tgts |> Seq.map(fun v -> v:?>IVertex)
-        let targetIncommingResets = 
-            targetSegs
-            |> Seq.map(fun ts ->
-                getIncomingEdges (getAdjacencyGraphFromEdge (ts:?>Segment).ContainerFlow.Edges) ts
-            )
 
-        targetIncommingResets
+        targetSegs
+        |> Seq.map(fun ts ->
+            getIncomingEdges (getAdjacencyGraphFromEdge (ts:?>Segment).ContainerFlow.Edges) ts
+        )
         |> Seq.map(fun es ->
             es
             |> Seq.filter(fun e ->
                 e.OriginalEdge.Operator = "|>" || e.OriginalEdge.Operator = "|>>"
             )
-        )
-        |> Seq.map(fun es ->
-            es
             |> Seq.map(fun e ->
                 findGraphIncludeSource e.Source
             )
         )
         |> Seq.collect id
-        |> Seq.collect(fun rv ->
-            rv 
-            |> Seq.filter(fun r ->
-                Enumerable.SequenceEqual(Enumerable.Intersect(sourceSegs, r), sourceSegs)
-            )
+        |> Seq.filter(fun rv ->
+            Enumerable.SequenceEqual(Enumerable.Intersect(sourceSegs, fst(rv)), sourceSegs)
         )
 
     let searchCallTargets(cpts:CallPrototype seq) (tgt:CallPrototype) = 
@@ -193,11 +199,11 @@ module GraphUtil =
         |> Seq.iter(fun src->
             targetResetMarker src.TXs tgt.RXs
             |> Seq.iter(fun v ->
-                printfn "'%A' strongly resets '%A' in progress of graph %A" src tgt v
+                printfn "'%A' strongly resets '%A' in progress of graph %A" src tgt (snd(v))
             )
         )
 
-    let checkCallMutualReset(task:Task seq) =
+    let checkCallResetSource(task:Task seq) =
         task
         |> Seq.iter(fun t ->
             let cpts = t.CallPrototypes

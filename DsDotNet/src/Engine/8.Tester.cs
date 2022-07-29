714537179e47b06bfadd28d22c250da43e96fb85
= <=>
2022-07-29 18:13:11 +0900

    calculation theta in progress -ongoing....

--- DsDotNet/src/Engine/8.Tester.cs
+++ DsDotNet/src/Engine/8.Tester.cs
@@ -7,6 +7,70 @@ class Tester
 {
     public static void DoSampleTest()
     {
+        /*
+        var text = @"
+//[sys] L = {
+//    [task] T = {
+//        Cp = {P.F.Vp ~ P.F.Sp}
+//        Cm = {P.F.Vm ~ P.F.Sm}
+//    }
+//    [flow] F = {
+//        Main = { T.Cp > T.Cm; }
+//    }
+//}
+
+[sys] L = {
+    [alias] = {
+        P.F.Vp = { Vp1; Vp2; Vp3; }
+        P.F.Vm = { Vm1; Vm2; Vm3; }
+        L.T.Cp = {Cp1; Cp2; Cp3;}
+        L.T.Cm = {Cm1; Cm2; Cm3;}
+    }
+    [task] T = {
+        Cp = {P.F.Vp ~ P.F.Sp}
+        Cm = {P.F.Vm ~ P.F.Sm}
+        //Cm1 = {P.F.Vm ~ P.F.Sm, P.F.Sm}
+        //Cm2 = {P.F.Vm ~ P.F.Sm}
+        //Cm3 = {P.F.Vm ~ P.F.Sm}
+        //Cp2 = {P.F.Vp ~ P.F.Sp}
+    }
+    [flow] F = {
+        //Main = { T.Cp |> T.Cm, T.Cm1 > T.Cm2; T.Cm3 > T.Cm2; Cp2 > Cm2}
+        //Main = { T.Cp2 |> Cp2; }
+        //Main = { Cp2 |> Cm2; }
+        Main = { T.Cp > T.Cm; }
+        //Main > Weak;
+        //Cp1 > Cm1;
+        //Weak >> Strong;
+        //Main |> XXX;
+        //parenting = {A > B > C; C |> B; }
+        //T.C1 <||> T.C2;
+        //A, B > C > D, E;
+        //T.Cm > T.Cp;
+        //T.Cm |> T.Cp;
+    }
+}
+[sys] P = {
+    [flow] F = {
+        Vp > Pp > Sp;
+        Vm > Pm > Sm;
+
+        Pp |> Sm;
+        Pm |> Sp;
+        Vp <||> Vm;
+    }
+}
+[cpus] AllCpus = {
+    [cpu] Cpu = {
+        L.F;
+    }
+    [cpu] OtherCpu = {
+        P.F;
+    }
+}
+
+";
+        */
         var text = @"
 [sys] L = {
     [alias] = {
@@ -63,11 +127,11 @@ class Tester
         //    T.Cp2 <||> T.Cm2;
         //}
 
-        ////TC3.1
-        //tester = {
-        //    T.Cp1 > T.Cp2 > T.Cm2 > T.Cm1;
-        //    T.Cp1 |> T.Cm2 |> T.Cp2 |> T.Cm1;
-        //}
+        //TC3.1
+        tester = {
+            T.Cp1 > T.Cp2 > T.Cm2 > T.Cm1;
+            T.Cp1 |> T.Cm2 |> T.Cp2 |> T.Cm1;
+        }
 
         ////TC4
         //tester = {

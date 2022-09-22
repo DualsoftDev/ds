7d9f071d5970b054be70f1702febff25691feb2e
ahn <ahn@dualsoft.com>
2022-09-22 11:47:40 +0900

    .

---
+++ DsDotNet/src/Engine.Base/CoreGraph.fs
@@ -0,0 +1,21 @@
+// Copyright (c) Dual Inc.  All Rights Reserved.
+namespace Engine.Core
+
+open System.Diagnostics
+open System.Collections.Generic
+open System.Collections.Concurrent
+
+[<AutoOpen>]
+module CoreGraph =
+
+    [<DebuggerDisplay("{name}")>]
+    type DsGraph(name:string)  =
+        inherit SysBase(name)
+        let dicRootFlow = ConcurrentDictionary<string, RootFlow>()
+      
+        member x.RootFlows() = dicRootFlow.Values
+        member x.AddFlow(flow:RootFlow) = dicRootFlow.TryAdd(flow.Name, flow);
+        member x.GetFlow(name:string)   = dicRootFlow.[name];
+        //나의 시스템 Flag
+        member val Active = false
+        

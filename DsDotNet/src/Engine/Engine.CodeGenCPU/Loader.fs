56aaee0495390c1821051cda8875a8bb32ca08fe
ahn <ahn@dualsoft.com>
2022-12-14 16:32:58 +0900

    .

--- DsDotNet/src/Engine/Engine.CodeGenCPU/Loader.fs
+++ DsDotNet/src/Engine/Engine.CodeGenCPU/Loader.fs
@@ -7,13 +7,13 @@ open Engine.Common.FS
 [<AutoOpen>]
 module CpuLoader =
 
-    ///CPU에 DsSystem 규격으로 code 불러 로딩하기
+    ///DsSystem 규격으로 cpu code 불러 로딩하기
     let LoadStatements(system:DsSystem) = 
         let statements, dicMemory =  ConvertSystem(system)
 
         statements, dicMemory
 
-    ///CPU에 DsSystem 규격으로 code 불러 Text으로 리턴
+    ///DsSystem 규격으로 cpu code 불러 Text으로 리턴
     let LoadStatementsForText(system:DsSystem) = 
         let statements, dicMemory =  ConvertSystem(system)
 

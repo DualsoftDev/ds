a44f1b39d23bd752c0536c177998335e79f602ba
ahn <ahn@dualsoft.com>
2022-12-14 16:28:36 +0900

    LoadStatementsForText Cpu 생성 코드

--- DsDotNet/src/Engine/Engine.CodeGenCPU/Loader.fs
+++ DsDotNet/src/Engine/Engine.CodeGenCPU/Loader.fs
@@ -1,18 +1,23 @@
 namespace Engine.CodeGenCPU
 
-open System.Collections.Concurrent
-open System.IO
 open System.Linq
 open Engine.Core
-open System
 open Engine.Common.FS
 
 [<AutoOpen>]
 module CpuLoader =
 
     ///CPU에 text 규격으로 code 불러 로딩하기
-    let LoadStatements(system:DsSystem) =
-        let statements =  ConvertSystem(system)
+    let LoadStatements(system:DsSystem) = 
+        let statements, dicMemory =  ConvertSystem(system)
+
+        statements, dicMemory
+
+    ///CPU에 text 규격으로 code 불러 Text으로 리턴
+    let LoadStatementsForText(system:DsSystem) = 
+        let statements, dicMemory =  ConvertSystem(system)
 
         statements
+            .Select(fun (desc, statement) -> statement.ToText())
+            .JoinWith("\r\n")
 

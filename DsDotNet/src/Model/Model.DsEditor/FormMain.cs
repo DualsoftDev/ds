3db7ff00b473f875bc3d2943ff0702b09fa2dcbb
ahn <ahn@dualsoft.com>
2022-12-21 18:43:09 +0900

    Model.DsEditor 추가

---
+++ DsDotNet/src/Model/Model.DsEditor/FormMain.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Model.DsEditor
+{
+    public partial class FormMain : Form
+    {
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DsDotNet/src/Model/Model.DsEditor/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Model.DsEditor
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var form = new FormMain();
+            Application.Run(form);
+
+        }
+    }
+
+
+
+}

557e937645a063b80a05ee5cdc85bc9d3ae35884
kwak <kwak@dualsoft.com>
2023-01-03 13:10:37 +0900

    - XGI PLC : Rising/Falling/Negation

--- DsDotNet/src/UnitTest/UnitTest.PLC.Xgi/RisingFallingTest.fs
+++ DsDotNet/src/UnitTest/UnitTest.PLC.Xgi/RisingFallingTest.fs
@@ -1,6 +1,5 @@
 namespace T
 
-
 open NUnit.Framework
 
 open Engine.Parser.FS
@@ -18,10 +17,13 @@ type XgiRisingFallingTest() =
         let code = """
             bool ix = createTag("%IX0.0.0", false);
             bool qx = createTag("%QX0.1.0", false);
-            $qx := $ix && not($ix) && rising($ix) && falling($ix);
+            $qx := $ix && ! $ix && rising($ix) && falling($ix);
 """
+
         let statements = parseCode storages code
+        statements.Length === 1
+        statements[0].ToText() === "$qx := $ix && !($ix) && rising($ix) && falling($ix)"
+
         let xml = LsXGI.generateXml plcCodeGenerationOption storages (map withNoComment statements)
         saveTestResult (get_current_function_name()) xml
-
 

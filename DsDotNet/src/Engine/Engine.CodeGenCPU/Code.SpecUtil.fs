ae14b0ecc9b0f006ec9cf62beec218ceb70fcab5
kwak <kwak@dualsoft.com>
2023-01-17 09:52:26 +0900

    - Class Hierachies pptx documents added.

--- DsDotNet/src/Engine/Engine.CodeGenCPU/Code.SpecUtil.fs
+++ DsDotNet/src/Engine/Engine.CodeGenCPU/Code.SpecUtil.fs
@@ -15,24 +15,25 @@ module CodeSpecUtil =
 
     [<Flags>]
     [<AutoOpen>]
-    type ConvertType = 
-    |RealInFlow          = 0b00000001  
-    |RealExFlow          = 0b00000010  
-    |CallInFlow          = 0b00000100  
-    |CallInReal          = 0b00001000  
-    |AliasCallInReal     = 0b00010000  
-    |AliasCallInFlow     = 0b00100000  
-    |AliasRealInFlow     = 0b01000000  
-    |AliasRealExInFlow   = 0b10000000  
+    type ConvertType =
+        | RealInFlow          = 0b00000001
+        | RealExFlow          = 0b00000010
+        | CallInFlow          = 0b00000100
+        | CallInReal          = 0b00001000
+        | AliasCallInReal     = 0b00010000
+        | AliasCallInFlow     = 0b00100000
+        | AliasRealInFlow     = 0b01000000
+        | AliasRealExInFlow   = 0b10000000
 
-    |InFlowWithoutReal   = 0b11100110 
-    |InFlowAll           = 0b11100111 
-    |CoinTypeAll         = 0b11111110 
-    |CallTypeAll         = 0b00001100 
-    |RealNIndirectReal   = 0b11000011 
-    |VertexAll           = 0b11111111 
-   
-    let IsSpec (v:Vertex) (vaild:ConvertType) = 
+    let InFlowWithoutReal = AliasRealExInFlow ||| AliasRealInFlow ||| AliasCallInFlow                                    ||| CallInFlow ||| RealExFlow                    // 0b11100110
+    let InFlowAll         = AliasRealExInFlow ||| AliasRealInFlow ||| AliasCallInFlow                                    ||| CallInFlow ||| RealExFlow ||| RealInFlow     // 0b11100111
+    let CoinTypeAll       = AliasRealExInFlow ||| AliasRealInFlow ||| AliasCallInFlow ||| AliasCallInReal ||| CallInReal ||| CallInFlow ||| RealExFlow                    // 0b11111110
+    let CallTypeAll       =                                                                                   CallInReal ||| CallInFlow                                   // 0b00001100
+    let RealNIndirectReal = AliasRealExInFlow ||| AliasRealInFlow                                                                       ||| RealExFlow ||| RealInFlow     // 0b11000011
+    let VertexAll         = AliasRealExInFlow ||| AliasRealInFlow ||| AliasCallInFlow ||| AliasCallInReal ||| CallInReal ||| CallInFlow ||| RealExFlow ||| RealInFlow     // 0b11111111
+
+
+    let IsSpec (v:Vertex) (vaild:ConvertType) =
         let isVaildVertex =
             match v with
             | :? Real   -> vaild.HasFlag(RealInFlow)

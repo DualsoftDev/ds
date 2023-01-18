ba18668d871f69391b47c72e135135c6ca0fa0bb
kwak <kwak@dualsoft.com>
2023-01-18 17:57:39 +0900

    .

--- DsDotNet/src/Engine/Engine.Common.FS/ComputationExpressions/StateBuilder.fs
+++ DsDotNet/src/Engine/Engine.Common.FS/ComputationExpressions/StateBuilder.fs
@@ -3,27 +3,27 @@ namespace Engine.Common.FS
 open System
 open System.IO
 
-// https://tomasp.net/blog/2014/update-monads/
-
-
 // https://dev.to/shimmer/the-state-monad-in-f-3ik0
 // https://gist.github.com/jwosty/5338fce8a6691bbd9f6f
+
+// https://tomasp.net/blog/2014/update-monads/
+
 [<AutoOpen>]
 module StateBuilderModule =
-    type State<'s, 'a> = State of ('s -> ('a * 's))
+    type State<'Z, 'A> = State of ('Z -> ('A * 'Z))
 
     module State =
-        let inline run state (State f) = f state
-        let ret result = State(fun state -> (result, state))
-        let bind binder stateful =
-            State(fun state ->
-                let result, state' = stateful |> run state
-                binder result |> run state')
-        let get = State(fun s -> s, s)
-        let put newState = State(fun _ -> (), newState)
-        let map f s = State(fun (state: 's) ->
-            let x, state = run state s
-            f x, state)
+        let inline run z (State f) = f z
+        let ret a = State(fun state -> (a, state))
+        let bind binder state =
+            State(fun z ->
+                let a, z' = state |> run z
+                binder a |> run z')
+        let get = State(fun z -> z, z)
+        let put z = State(fun _ -> (), z)
+        let map f s = State(fun z ->
+            let a, z' = run z s
+            f a, z')
 
     /// The state monad passes around an explicit internal state that can be
     /// updated along the way. It enables the appearance of mutability in a purely
@@ -31,25 +31,25 @@ module StateBuilderModule =
     /// (in StateBuilder()). In other words, you implicitly pass around an implicit
     /// state that gets transformed along its journey through pipelined code.
     type StateBuilder() =
-        member this.Zero () = State(fun s -> (), s)
-        member this.Return x = State(fun s -> x, s)
-        member inline this.ReturnFrom (x: State<'s, 'a>) = x
-        member this.Bind (x, f) : State<'s, 'b> =
-            State(fun state ->
-                let (result: 'a), state = State.run state x
-                State.run state (f result))
-        member this.Combine (x1: State<'s, 'a>, x2: State<'s, 'b>) =
-            State(fun state ->
-                let result, state = State.run state x1
-                State.run state x2)
-        member this.Delay f : State<'s, 'a> = f ()
-        member this.For (seq, (f: 'a -> State<'s, 'b>)) =
+        member _.Zero() = State(fun z -> (), z)
+        member _.Return a = State(fun z -> a, z)
+        member inline _.ReturnFrom(state: State<'Z, 'A>) = state
+        member _.Bind(state, f) : State<'Z, 'A> =
+            State(fun z ->
+                let (b:'B), z' = State.run z state
+                State.run z' (f b))
+        member _.Combine(state1: State<'Z, 'A>, state2: State<'Z, 'B>) =
+            State(fun z ->
+                let a_, z' = State.run z state1
+                State.run z' state2)
+        member _.Delay f : State<'Z, 'A> = f ()
+        member x.For(seq, (f: 'A -> State<'Z, 'B>)) =
             seq
             |> Seq.map f
-            |> Seq.reduceBack (fun x1 x2 -> this.Combine (x1, x2))
-        member this.While (f, x) =
-            if f () then this.Combine (x, this.While (f, x))
-            else this.Zero ()
+            |> Seq.reduceBack (fun state1 state2 -> x.Combine (state1, state2))
+        member x.While (f, state) =
+            if f() then x.Combine(state, x.While (f, state))
+            else x.Zero()
 
     let state = new StateBuilder()
 
@@ -60,20 +60,23 @@ module private ShowSamples =
         state {
             for i in 0..20 do
                 if i % 3 = 0 then
-                    let! s = State.get
-                    do! State.put (s + i)
+                    let! z = State.get
+                    do! State.put (z + i)
             return! State.get
         } |> State.run 0 |> fst
     let verify c = if not c then failwith "ERROR"
-    verify ([ 3..3..20 ] |> List.sum = sumOfMultiplesOfThreeUptoTwenty)
+    let verifyM (message:string) condition =
+        if not condition then
+            failwith message
+    verify ( sumOfMultiplesOfThreeUptoTwenty = ([ 3..3..20 ] |> List.sum) )
 
     module SeqBased =
-        /// Cons the input to the state
-        let push x = State(fun (s: 's seq) -> (), Seq.append [x] s)
+        /// Cons the input to the state.  'Z = 's seq
+        let push x = State(fun (xs: 's seq) -> (), Seq.append [x] xs)
         /// Update and remove the return value from the top of the stack
-        let pop = State(fun (s: 's seq) -> Seq.head s, Seq.tail s)
+        let pop = State(fun (xs: 's seq) -> Seq.head xs, Seq.tail xs)
         /// Update the return value to the top of the stack
-        let peek = State(fun (s: 's seq) -> Seq.head s, s)
+        let peek = State(fun (xs: 's seq) -> Seq.head xs, xs)
 
         module Seq =
             /// Create a safe lazy infinite sequence of random integers using a System.Random
@@ -92,18 +95,18 @@ module private ShowSamples =
             } |> State.run (Seq.fromRandom (new Random()))
 
     module ListBased =
-        /// Cons the input to the state
-        let push x = State(fun (s: 's list) -> (), x::s)
+        /// Cons the input to the state.  'Z = 's list
+        let push x = State(fun (xs: 's list) -> (), x::xs)
         /// Update and remove the return value from the top of the stack
-        let pop = State(fun (s: 's list) ->
-            match s with
+        let pop = State(fun (xs: 's list) ->
+            match xs with
             | [] -> failwith "Stack is empty"
             | h::t -> h, t)
         /// Update the return value to the top of the stack
-        let peek = State(fun (s: 's list) ->
-            match s with
+        let peek = State(fun (xs: 's list) ->
+            match xs with
             | [] -> failwith "Stack is empty"
-            | h::t -> h, s)
+            | h::t -> h, xs)
 
 
         let comp =
@@ -118,13 +121,114 @@ module private ShowSamples =
             }
         // Output: (9, Stack [8; 3; 0; 2; 1; 0])
         let stack = [9; 0; 2; 1; 0]
-        printfn "%A" (State.run stack comp)
+        let a, z = State.run stack comp
+        verify (a = 9)
+        verify (z = [8; 3; 0; 2; 1; 0])
 
         // Output: (5, Stack [7; 1])
         let stack2 = [5; 1]
-        printfn "%A" (State.run stack2 comp)
+        let a2, z2 = State.run stack2 comp
+        verify (a2 = 5)
+        verify (z2 = [7; 1])
+
+    module TurtleGraphic =
+        type Coord = int*int
+        let withHistory =
+            let up n : State<Coord list, Coord> =
+                State(fun (xs: Coord list) ->
+                    match xs with
+                    | (x, y)::t -> (x, y+n), (x, y+n)::xs
+                    | [] -> failwith "ERROR")
+            let down n : State<Coord list, Coord> =
+                State(fun (xs: Coord list) ->
+                    match xs with
+                    | (x, y)::t -> (x, y-n), (x, y-n)::xs
+                    | [] -> failwith "ERROR")
+            let right n : State<Coord list, Coord> =
+                State(fun (xs: Coord list) ->
+                    match xs with
+                    | (x, y)::t -> (x+n, y), (x+n, y)::xs
+                    | [] -> failwith "ERROR")
+            let left n : State<Coord list, Coord> =
+                State(fun (xs: Coord list) ->
+                    match xs with
+                    | (x, y)::t -> (x-n, y), (x-n, y)::xs
+                    | [] -> failwith "ERROR")
+            let get = State(fun (xs: Coord list) -> xs|> List.head, xs)
+            let put x = State(fun xs -> (), x::xs)
 
 
+            let graphic =
+                state {
+                    let! c = get
+                    verify(c = (0, 0))
+
+                    let! c = up 1
+                    verify(c = (0, 1))
+
+                    let! c = right 3
+                    verify(c = (3, 1))
+
+                    let! c = down 1
+                    verify(c = (3, 0))
+
+                    let! c = left 3
+                    verify(c = (0, 0))
+
+                    do! put (10, 10)
+                    let! c = get
+                    verify(c = (10, 10))
+
+                    let! c = up 3
+                    verify(c = (10, 13))
+
+                    return! State.get
+                } |> State.run [ 0, 0 ] |> fst |> List.rev
+            verify (graphic = [(0, 0); (0, 1); (3, 1); (3, 0); (0, 0); (10, 10); (10, 13)])
+            graphic
+
+
+        let withoutHistory =
+            let up n : State<Coord, Coord> =
+                State(fun ((x, y): Coord) -> (x, y+n), (x, y+n))
+            let down n : State<Coord, Coord> =
+                State(fun ((x, y): Coord) -> (x, y-n), (x, y-n))
+            let right n : State<Coord, Coord> =
+                State(fun ((x, y): Coord) -> (x+n, y), (x+n, y))
+            let left n : State<Coord, Coord> =
+                State(fun ((x, y): Coord) -> (x-n, y), (x-n, y))
+            let get = State(fun ((x, y): Coord) -> (x, y), (x, y))
+            let put ((x, y): Coord) = State(fun z -> (), (x, y))
+
+
+            let graphic =
+                state {
+                    let! c = get
+                    verify(c = (0, 0))
+
+                    let! c = up 1
+                    verify(c = (0, 1))
+
+                    let! c = right 3
+                    verify(c = (3, 1))
+
+                    let! c = down 1
+                    verify(c = (3, 0))
+
+                    let! c = left 3
+                    verify(c = (0, 0))
+
+                    do! put (10, 10)
+                    let! c = get
+                    verify(c = (10, 10))
+
+                    let! c = up 3
+                    verify(c = (10, 13))
+
+                    return! get
+                } |> State.run (0, 0) |> fst
+            verify (graphic = (10, 13))
+            graphic
 
     // Some utility types and functions for the next state monad example
 

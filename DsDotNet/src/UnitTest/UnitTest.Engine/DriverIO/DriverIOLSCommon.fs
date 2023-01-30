64526cdcdedab6524bc4336ee68240d31a8b1735
ahn <ahn@dualsoft.com>
2023-01-30 19:30:23 +0900

    LS Connection test

--- DsDotNet/src/UnitTest/UnitTest.Engine/DriverIO/DriverIOLSCommon.fs
+++ DsDotNet/src/UnitTest/UnitTest.Engine/DriverIO/DriverIOLSCommon.fs
@@ -11,13 +11,24 @@ open Engine.Common.FS
 open Engine.Cpu
 open Engine.CodeGenCPU
 open System.Linq
+open Dsu.PLC.LS
 
 [<AutoOpen>]
 module DriverIOLSCommonTest =
 
     type DriverIOLSCommon() =
         inherit EngineTestBaseClass()
+        let t = CpuTestSample()
 
-        //let _conn =  LsConnection()
+        let conn = new LsConnection(new LsConnectionParameters("192.168.0.100", 2004us, Dsu.PLC.TransportProtocol.Udp, 3000.0))
 
+        member x.Connection    =  conn
 
+        [<Test>]
+        member __.``XXXXXXXXX LS Connection test`` () =
+            let ls = DriverIOLSCommon()
+            let conn = ls.Connection
+            conn.PerRequestDelay <- 1000
+            if conn.Connect()
+            then true
+            else false

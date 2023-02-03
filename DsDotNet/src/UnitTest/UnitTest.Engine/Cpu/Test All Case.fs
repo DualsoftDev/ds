5a4b82e442ef395f5b74d705027a4b41477abe93
ahn <ahn@dualsoft.com>
2023-02-03 18:54:58 +0900

    .

--- DsDotNet/src/UnitTest/UnitTest.Engine/Cpu/Test All Case.fs
+++ DsDotNet/src/UnitTest/UnitTest.Engine/Cpu/Test All Case.fs
@@ -41,21 +41,3 @@ type TestAllCase() =
         result === result
 
 
-    [<Test>]
-    member __.``help kwak XXXX 성능 개선중 Model Origin Cpu test``    () =
-        //GetOriginsWithDeviceDefs (graph:DsGraph) 성능 개선중
-        //<kwak> help
-        //하나의 Real에 자식 Coin이 11ea 일 경우 기준 원위치 뽑는데 1초이상 걸리고
-        //real 4개가 제 PC 기준 8초 정도 걸리고 있습니다.
-        //시스템에 100개 real이 존재할경우... real 병렬처리 이전에 하나라도 좀 빨라지면
-        //<kwak> help
-        let f = getFuncName()
-        let sampleDirectory = Path.Combine($"{__SOURCE_DIRECTORY__}", "../../UnitTest.Model/ImportOfficeExample/sample/");
-        let pptPath = sampleDirectory + "s_car.pptx"
-        let model = ImportPPT.GetModel [ pptPath ]
-        model.Systems.ForEach(testAddressSetting)
-
-        let result = exportXMLforXGI(model.Systems.First(), myTemplate f, None)
-        //추후 정답과 비교 필요
-        result === result
-

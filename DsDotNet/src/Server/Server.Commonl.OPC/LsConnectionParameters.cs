7037147e99cbea04c34a469cb705271302d509e4
ikshin <ikshin@dualsoft.com>
2023-02-03 13:55:41 +0900

    changer server projects

---
+++ DsDotNet/src/Server/Server.Commonl.OPC/LsConnectionParameters.cs
@@ -0,0 +1,22 @@
+//추후 참조
+//using Dsu.PLC;
+//using Dsu.PLC.LS;
+//using Dsu.PLC.Common;
+
+using Microsoft.FSharp.Core;
+
+namespace Server.Common.OPC;
+
+internal class LsConnectionParameters
+{
+    private string v1;
+    private FSharpOption<ushort> fSharpOption;
+    private double v2;
+
+    public LsConnectionParameters(string v1, FSharpOption<ushort> fSharpOption, double v2)
+    {
+        this.v1 = v1;
+        this.fSharpOption = fSharpOption;
+        this.v2 = v2;
+    }
+}

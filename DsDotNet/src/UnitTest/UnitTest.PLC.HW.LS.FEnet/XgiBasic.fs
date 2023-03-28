e88d9ed377b5a983e5e8735a5855df410ed1e876
dskim9752 <dskim9752@dualsoft.com>
2023-03-28 12:12:31 +0900

    Add xgi read test

--- DsDotNet/src/UnitTest/UnitTest.PLC.HW.LS.FEnet/XgiBasic.fs
+++ DsDotNet/src/UnitTest/UnitTest.PLC.HW.LS.FEnet/XgiBasic.fs
@@ -8,7 +8,14 @@ open System.Text.RegularExpressions
 open Dsu.PLC.LS
 
 type XgiBasic() =
-    inherit FEnetTestBase("192.168.0.111")
+    inherit FEnetTestBase("192.168.0.100")
+
+    let LAddress = 512      // 모든 L주소 512에 최대값 넣기(A 제외)
+    let DAddress = LAddress * 2
+    let WAddress = DAddress * 2
+    let BAddress = WAddress * 2
+    let bitAddress = BAddress * 8
+
 
     member private x.Write(tag, value) =
         let lsTag = LsTagXgi(x.Conn, tag)
@@ -35,6 +42,105 @@ type XgiBasic() =
         x.Read("%MW3") === 0xFFFFus
 
         x.Read("%ML0") === 0xFFFFFFFFFFFFFFFFUL
+
+
+
+    [<Test>]
+    member x.``Readings All Memory bit type`` () =
+        (* PLC 에서 %_B11 을 FF 값으로 채우고 테스트. 단, F와 A는 0으로 고정됨 *)
+        let memoryType = "X"
+        let address = bitAddress
+                    |>toString
+        let answer = true
+        x.Read("%M"+memoryType+address) === answer
+        x.Read("%L"+memoryType+address) === answer
+        x.Read("%N"+memoryType+address) === answer
+        x.Read("%K"+memoryType+address) === answer
+        x.Read("%R"+memoryType+address) === answer
+        x.Read("%W"+memoryType+address) === answer
+        x.Read("%A"+memoryType+address) =!= answer
+        x.Read("%F"+memoryType+address) === answer 
+        x.Read("%U"+memoryType+address) === answer          
+        //x.Read("%I"+memoryType+address) === answer   
+        //x.Read("%Q"+memoryType+address) === answer 
+
+    [<Test>]
+    member x.``Readings All Memory byte type`` () =
+        (* PLC 에서 %_B11 을 FF 값으로 채우고 테스트. 단, F와 A는 0으로 고정됨 *)
+        let memoryType = "B"
+        let address = BAddress
+                    |>toString
+        let answer = 0xFFuy
+        x.Read("%M"+memoryType+address) === answer
+        x.Read("%L"+memoryType+address) === answer
+        x.Read("%N"+memoryType+address) === answer
+        x.Read("%K"+memoryType+address) === answer
+        x.Read("%R"+memoryType+address) === answer
+        x.Read("%W"+memoryType+address) === answer
+        x.Read("%A"+memoryType+address) === answer - answer
+        x.Read("%F"+memoryType+address) === answer 
+        x.Read("%U"+memoryType+address) === answer          
+        //x.Read("%I"+memoryType+address) === answer   
+        //x.Read("%Q"+memoryType+address) === answer           
+
+    [<Test>]
+    member x.``Readings All Memory word type`` () =
+        (* PLC 에서 %_W11 을 FF 값으로 채우고 테스트. 단, F와 A는 0으로 고정됨 *)
+        let memoryType = "W"
+        let address = WAddress
+                    |>toString
+
+        let answer = 0xFFFFus
+        x.Read("%M"+memoryType+address) === answer
+        x.Read("%L"+memoryType+address) === answer
+        x.Read("%N"+memoryType+address) === answer
+        x.Read("%K"+memoryType+address) === answer
+        x.Read("%R"+memoryType+address) === answer
+        x.Read("%W"+memoryType+address) === answer
+        x.Read("%A"+memoryType+address) === answer - answer
+        x.Read("%F"+memoryType+address) === answer
+        x.Read("%U"+memoryType+address) === answer          
+        //x.Read("%I"+memoryType+address) === answer   
+        //x.Read("%Q"+memoryType+address) === answer      
+
+    [<Test>]
+    member x.``Readings All Memory Double word type`` () =
+        (* PLC 에서 %_W11 을 FF 값으로 채우고 테스트. 단, F와 A는 0으로 고정됨 *)
+        let memoryType = "D"
+        let address = DAddress
+                    |>toString
+        let answer = 0xFFFFFFFFu
+        x.Read("%M"+memoryType+address) === answer
+        x.Read("%L"+memoryType+address) === answer
+        x.Read("%N"+memoryType+address) === answer
+        x.Read("%K"+memoryType+address) === answer
+        x.Read("%R"+memoryType+address) === answer
+        x.Read("%W"+memoryType+address) === answer
+        x.Read("%A"+memoryType+address) === answer - answer
+        x.Read("%F"+memoryType+address) === answer
+        x.Read("%U"+memoryType+address) === answer          
+        //x.Read("%I"+memoryType+address) === answer   
+        //x.Read("%Q"+memoryType+address) === answer          
+
+
+    [<Test>]
+    member x.``Readings All Memory Long word type`` () =
+        (* PLC 에서 %_W11 을 FF 값으로 채우고 테스트. 단, A는 0으로 고정됨 *)
+        let memoryType = "L"
+        let address = LAddress
+                    |>toString
+        let answer = 0xFFFFFFFFFFFFFFFFUL
+        x.Read("%M"+memoryType+address) === answer
+        x.Read("%L"+memoryType+address) === answer
+        x.Read("%N"+memoryType+address) === answer
+        x.Read("%K"+memoryType+address) === answer
+        x.Read("%R"+memoryType+address) === answer
+        x.Read("%W"+memoryType+address) === answer
+        x.Read("%A"+memoryType+address) === answer - answer
+        x.Read("%F"+memoryType+address) === answer
+        x.Read("%U"+memoryType+address) === answer          //4.0.0    
+        //x.Read("%I"+memoryType+address) === answer   
+        //x.Read("%Q"+memoryType+address) === answer        
 
 
     [<Test>]

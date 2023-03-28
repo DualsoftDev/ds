4b52063633145db7264f908aa0c9d6e42025912d
kwak <kwak@dualsoft.com>
2023-03-28 10:43:37 +0900

    .

--- DsDotNet/src/UnitTest/UnitTest.PLC.HW.LS.FEnet/XgbMkBasic.fs
+++ DsDotNet/src/UnitTest/UnitTest.PLC.HW.LS.FEnet/XgbMkBasic.fs
@@ -10,14 +10,6 @@ type XgbMkBasic() =
 
     override x.CreateLsTag (tag:string) (convertFEnet:bool) =
         LsTagXgbMk(x.Conn, tag, convertFEnet)
-    //member private x.WriteTagValue(tag, value, convertFEnet) =
-    //    let lsTag = LsTagXgbMk(x.Conn, tag, convertFEnet)
-    //    lsTag.Value <- value
-    //    x.Conn.WriteATag(lsTag) |> ignore
-    //member private x.Write(tag, value) = x.WriteTagValue(tag, value, true)
-    //member private x.WriteFEnet(tag, value) = x.WriteTagValue(tag, value, false)
-    //member private x.Read(tag:string) = x.Conn.ReadATag(tag)
-    //member private x.ReadFEnet(tag:string) = x.Conn.ReadATagFEnet(tag)
 
     [<Test>]
     member x.``Connection Check`` () =
@@ -50,21 +42,6 @@ type XgbMkBasic() =
 
         (fun () -> x.Write("%MW5", 0us)       |> ignore ) |> ShouldFail
         (fun () -> x.WriteFEnet("M0005", 0us) |> ignore ) |> ShouldFail
-
-    //[<Test>]
-    //member x.``Readings`` () =
-    //    (* PLC 에서 %ML0 를 FF 값으로 채우고 있다는 가정하에... *)
-    //    let mb0 = x.Conn.ReadATag("%MB0")
-    //    mb0 === 0xFFuy
-    //    x.Read("%MB1") === 0xFFuy
-    //    x.Read("%MB7") === 0xFFuy
-
-    //    x.Read("%MW0") === 0xFFFFus
-    //    x.Read("%MW1") === 0xFFFFus
-    //    x.Read("%MW2") === 0xFFFFus
-    //    x.Read("%MW3") === 0xFFFFus
-
-    //    x.Read("%ML0") === 0xFFFFFFFFFFFFFFFFUL
 
 
     [<Test>]
@@ -110,8 +87,13 @@ type XgbMkBasic() =
 
     [<Test>]
     member x.``M with native`` () =
-        x.Write("M0032", 0xFFFFus)
-        x.Read("M0032") === 0xFFFFus
-        x.ReadFEnet("%MW32") === 0xFFFFus
+        let mutable w = 0x1234us
+        x.Write("M0032", w)
+        x.Read("M0032") === w
+        x.ReadFEnet("%MW32") === w
 
+        w <- 0x4321us
+        x.WriteFEnet("%MW32", w)
+        x.Read("M0032") === w
+        x.ReadFEnet("%MW32") === w
 
--- DsDotNet/src/UnitTest/UnitTest.PLC.HW.LS.FEnet/XgiBasic.fs
+++ DsDotNet/src/UnitTest/UnitTest.PLC.HW.LS.FEnet/XgiBasic.fs
@@ -13,15 +13,6 @@ type XgiBasic() =
     override x.CreateLsTag (tag:string) (convertFEnet:bool) =
         LsTagXgi(x.Conn, tag, convertFEnet)
 
-    //member private x.WriteTagValue(tag, value, convertFEnet) =
-    //    let lsTag = LsTagXgi(x.Conn, tag, convertFEnet)
-    //    lsTag.Value <- value
-    //    x.Conn.WriteATag(lsTag) |> ignore
-    //member private x.Write(tag, value) = x.WriteTagValue(tag, value, true)
-    //member private x.WriteFEnet(tag, value) = x.WriteTagValue(tag, value, false)
-    //member private x.Read(tag:string) = x.Conn.ReadATag(tag)
-    //member private x.ReadFEnet(tag:string) = x.Conn.ReadATagFEnet(tag)
-
     [<Test>]
     member x.``Connection Check`` () =
         let cpu = x.Conn.Cpu :?> LsCpu

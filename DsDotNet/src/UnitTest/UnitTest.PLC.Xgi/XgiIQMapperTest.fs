5a25ef5f1fb83603170670a2553f58ad3be1f2c1
kwak <kwak@dualsoft.com>
2023-03-30 06:40:57 +0900

    - Dummy I/Q map (alias) 처리

---
+++ DsDotNet/src/UnitTest/UnitTest.PLC.Xgi/XgiIQMapperTest.fs
@@ -0,0 +1,51 @@
+namespace T
+
+open NUnit.Framework
+
+open Engine.Parser.FS
+open Engine.Core
+open Engine.Common.FS
+open PLC.CodeGen.LSXGI
+open PLC.CodeGen.Common
+
+
+type IQMapperTest() =
+    inherit XgiTestBaseClass()
+
+
+    [<Test>]
+    member __.``Dummy IQ Map test`` () =
+        let globalStorages = Storages()
+        let pouIQMap =
+            let code = """
+                bool qAvante = false;
+                bool qSonata = false;
+                bool QCar = createTag("%QX1", false);
+                $QCar := $qAvante || $qSonata;
+
+                bool iSonata = false;
+                bool iAvante = false;
+                bool ICar = createTag("%IX1", false);
+                $iAvante := $ICar;
+                $iSonata := $ICar;
+    """
+            let statements = parseCode globalStorages code |> map withNoComment
+            {
+                TaskName = "Scan Program"
+                POUName = "POU1"
+                Comment = "POU1"
+                LocalStorages = Storages()
+                GlobalStorages = globalStorages
+                CommentedStatements = statements
+            }
+
+        let prjParams = {
+            defaultXgiProjectParams with
+                ProjectName = "Dummy IQ Map test"
+                GlobalStorages = globalStorages
+                POUs = [pouIQMap]
+        }
+
+        let xml = prjParams.GenerateXmlString()
+        let f = getFuncName()
+        saveTestResult f xml

837e9cdd81b5139bca29545d5082dfac9ff23b58
kwak <kwak@dualsoft.com>
2023-10-20 18:39:14 +0900

    - ObjectHolder (object serialization)

--- DsDotNet/src/UnitTest/UnitTest.Common/ObjectHolderTest.fs
+++ DsDotNet/src/UnitTest/UnitTest.Common/ObjectHolderTest.fs
@@ -36,16 +36,21 @@ module ObjectHolderTestModule =
         member _.ObjectHolderSerializeTest() =
 
             ObjectHolder.Create(1234567890123456789UL).GetValue().GetType() === typeof<uint64>
-            ObjectHolder.Create(123456789L).GetValue().GetType() === typeof<int64>
-            ObjectHolder.Create(123456789u).GetValue().GetType() === typeof<uint32>
-            ObjectHolder.Create(123456789).GetValue().GetType() === typeof<int32>
-            ObjectHolder.Create(1234us).GetValue().GetType() === typeof<uint16>
-            ObjectHolder.Create(1234s).GetValue().GetType() === typeof<int16>
-            ObjectHolder.Create(false).GetValue().GetType() === typeof<bool>
-            ObjectHolder.Create('a').GetValue().GetType() === typeof<char>
-            ObjectHolder.Create(255uy).GetValue().GetType() === typeof<byte>
+            ObjectHolder.Create(123456789L)           .GetValue().GetType() === typeof<int64>
+            ObjectHolder.Create(123456789u)           .GetValue().GetType() === typeof<uint32>
+            ObjectHolder.Create(123456789)            .GetValue().GetType() === typeof<int32>
+            ObjectHolder.Create(1234us)               .GetValue().GetType() === typeof<uint16>
+            ObjectHolder.Create(1234s)                .GetValue().GetType() === typeof<int16>
+            ObjectHolder.Create(false)                .GetValue().GetType() === typeof<bool>
+            ObjectHolder.Create('a')                  .GetValue().GetType() === typeof<char>
+            ObjectHolder.Create(255uy)                .GetValue().GetType() === typeof<byte>
+
 
             let un64 = ObjectHolder.Create(1234567890123456789UL)
+            let v = un64.GetValue()
+            let t = v.GetType()
+
+            let un64 = ObjectHolder.Create(9234567890123456789UL)
             let v = un64.GetValue()
             let t = v.GetType()
             
@@ -53,21 +58,57 @@ module ObjectHolderTestModule =
             let un64_ = JsonConvert.DeserializeObject<ObjectHolder>(strUn64)
             let v_ = un64_.GetValue()
 
+            /// v 를 serialize, deserialize 되었을 때의 type 이 원래 v 의 type 과 일치하는지 검사
+            let checkSerializedType v (t:System.Type) =
+                ObjectHolder.Create(v)
+                |> JsonConvert.SerializeObject
+                |> JsonConvert.DeserializeObject<ObjectHolder>
+                |> (fun x -> x.GetValue().GetType())
+                 === t
 
-            //let x64 = 1234567890123456789L
-            //let b64 = 1234567890123456789L |> box
-            //let xxx = b64 |> unbox<int64> |> uint64
+            checkSerializedType 1234567890123456789UL typeof<uint64>
+            checkSerializedType 9234567890123456789UL typeof<uint64>
+            checkSerializedType 1234149234567890123456789M typeof<Decimal>
+            checkSerializedType false typeof<bool>
+            checkSerializedType 'A' typeof<char>
+            checkSerializedType "Hello" typeof<string>
+            checkSerializedType 255uy typeof<byte>
+            checkSerializedType 255s typeof<int16>
+            checkSerializedType 255us typeof<uint16>
+            checkSerializedType 1234567890 typeof<int32>
+            checkSerializedType 1234567890u typeof<uint32>
+            checkSerializedType 12345678901234L typeof<int64>
+            checkSerializedType 3.14 typeof<double>
+            checkSerializedType 3.14f typeof<single>
 
-            //let nn64 = 1234567890L
-            //let xx64 = uint64 nn64 
+            /// v 를 serialize, deserialize 되었을 때의 원래 v 의 값과 일치하는지 검사
+            let checkSerializedValue v (t:System.Type) =
+                ObjectHolder.Create(v)
+                |> JsonConvert.SerializeObject
+                |> JsonConvert.DeserializeObject<ObjectHolder>
+                |> (fun x -> x.GetValue())
+                 === v
 
-            let nnn = 1234567890123456789UL
-            let un64 = ObjectHolder(nnn, ObjectHolderType.UInt64)
-            let strUn64 = JsonConvert.SerializeObject(un64)
-            let un64_ = JsonConvert.DeserializeObject<ObjectHolder>(strUn64)
-            let t = un64_.GetValue().GetType()
-            //t =!= typeof<int>
-            //t === typeof<JsonElement>
+            checkSerializedValue 1234567890123456789UL typeof<uint64>
+            checkSerializedValue 9234567890123456789UL typeof<uint64>
+            //checkSerializedValue 1234149234567890123456789M typeof<Decimal>     // 1234149234567890000000000M
+            checkSerializedValue false typeof<bool>
+            checkSerializedValue 'A' typeof<char>
+            checkSerializedValue "Hello" typeof<string>
+            checkSerializedValue 255uy typeof<byte>
+            checkSerializedValue 255s typeof<int16>
+            checkSerializedValue 255us typeof<uint16>
+            checkSerializedValue 1234567890 typeof<int32>
+            checkSerializedValue 1234567890u typeof<uint32>
+            checkSerializedValue 12345678901234L typeof<int64>
+            checkSerializedValue 3.14 typeof<double>
+            checkSerializedValue 3.14f typeof<single>
 
 
-            ()
+
+            let serialType = JsonConvert.SerializeObject >> JsonConvert.DeserializeObject<ObjectHolder> >> (fun x -> x.GetValue().GetType())
+            ObjectHolder.Create(1234567890123456789UL)
+            |> JsonConvert.SerializeObject
+            |> JsonConvert.DeserializeObject<ObjectHolder>
+            |> (fun x -> x.GetValue().GetType())
+             === typeof<uint64>

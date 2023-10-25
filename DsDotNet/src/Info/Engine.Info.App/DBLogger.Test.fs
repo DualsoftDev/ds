8b2c554a45ce577585191f77518d507d750046c0
kwak <kwak@dualsoft.com>
2023-10-26 08:11:09 +0900

    - [Database Logging] Optimize 진행 중.

---
+++ DsDotNet/src/Info/Engine.Info.App/DBLogger.Test.fs
@@ -0,0 +1,25 @@
+namespace Engine.Info
+
+open System
+open System.Configuration;
+open System.Collections.Concurrent
+open Dapper
+open Engine.Core
+open Microsoft.Data.Sqlite
+open Dual.Common.Core.FS
+open Dual.Common.Db
+open System.Collections.Generic
+open System.Data
+open System.Reactive.Disposables
+open System.Threading.Tasks
+open DBLoggerORM
+
+[<AutoOpen>]
+module internal DBLoggerTestModule =
+    let createTestLoggerInfoSetForReader(storages:Storage seq, ormLogs:ORMLog seq) : LoggerInfoSet =
+        let isReader = true
+        let logSet = LoggerInfoSet(storages, isReader)
+        logSet.InitializeForReader(ormLogs)
+        logSet
+
+

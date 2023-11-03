eab2839b2fb4cf002397d024de011c9ea823c387
kwak <kwak@dualsoft.com>
2023-11-03 14:35:47 +0900

    - ZeroMQ/NetMQ : Server -> Client Tag 변경 내역 공지 작업 중.  Samples

---
+++ DsDotNet/src/IOHub/ClientSamples/ClientSample.FSharp/client/Program.Client.FSharp.fs
@@ -0,0 +1,55 @@
+namespace Client
+
+open IO.Core
+open System
+open System.Threading
+open Dual.Common.Core.FS
+
+module ZmqTestMain =
+    [<EntryPoint>]
+    let main _ = 
+        let cts = new CancellationTokenSource()
+        let port = 5555
+        let client = new Client($"tcp://localhost:{port}")
+        let zmqInfo = Zmq.InitializeServer "zmqsettings.json"
+
+        let handleCancelKey (args: ConsoleCancelEventArgs) =
+            logDebug("Ctrl+C pressed!")
+            cts.Cancel()
+            args.Cancel <- true // 프로그램을 종료하지 않도록 설정 (선택 사항)
+        Console.CancelKeyPress.Add(handleCancelKey)
+
+
+
+        // ---- third party ----
+        // wb
+        let wr2 = client.WriteBytes("p/o", [|0; 1; 2; 3|], [|99uy; 98uy; 97uy; 96uy|])
+        match client.ReadBytes("p/o", [|0; 1; 2; 3|]) with
+        | Ok bytes ->
+            ()
+        | _ ->
+            failwith "ERROR"
+
+     
+        let mutable key = ""
+        while ( key <> null && not cts.IsCancellationRequested ) do
+            key <- Console.ReadLine()
+            if key <> null then
+                logDebug($"Got request [{key}]...")
+                match key with
+                | "q" | "Q" ->
+                    logDebug("Got quit request...")
+                    key <- null
+                | "" -> ()
+                | _ ->
+                    match client.SendRequest(key) with
+                    | Ok value ->
+                        Console.WriteLine($"OK: {value}")
+                    | Error err ->
+                        Console.WriteLine($"ERR: {err}")
+
+                ()
+
+        logDebug("Exiting...")
+        Thread.Sleep(1000)
+        0
---
+++ DsDotNet/src/IOHub/ClientSamples/ClientSample.FSharp/server/Program.Server.FSharp.fs
@@ -0,0 +1,81 @@
+namespace Server
+
+open IO.Core
+open System
+open System.Threading
+open Dual.Common.Core.FS
+
+module ZmqTestMain =
+    [<EntryPoint>]
+    let main _ = 
+        let zmqInfo = Zmq.InitializeServer "zmqsettings.json"
+        let server, client, cts = zmqInfo.Server, zmqInfo.Client, zmqInfo.CancellationTokenSource
+
+        use subs =
+            server.IOChangedObservable.Subscribe(fun change ->
+                for (tag, value) in change.GetTagNameAndValues() do
+                    logDebug $"Tag change detected on server side for {tag}: {value}"
+                ())
+
+        let handleCancelKey (args: ConsoleCancelEventArgs) =
+            logDebug("Ctrl+C pressed!")
+            cts.Cancel()
+            args.Cancel <- true // 프로그램을 종료하지 않도록 설정 (선택 사항)
+        Console.CancelKeyPress.Add(handleCancelKey)
+
+
+        //let rr0 = client.SendRequest("read Mw100 Mx30 Md12")
+        //let result = client.SendRequest("read Mw100 Mx30")
+        //let result2 = client.SendRequest("read Mw100 Mb70 Mx30 Md50 Ml50")
+        ////let result3 = client.SendRequest("read [Mw100..Mw30]")
+        //let wr = client.SendRequest("write Mw100=1 Mx30=false Md12=12")
+        //let rr = client.SendRequest("read Mw100 Mx30 Md12")
+        //let xxx = result
+
+        //// wb
+        //let wr2 = client.WriteBytes("M", [|0; 1; 2; 3|], [|0uy; 1uy; 2uy; 3uy|])
+        //let bytes:byte[] = client.ReadBytes("M", [|0; 1; 2; 3|])
+        //let words:uint16[] = client.ReadUInt16s("M", [|0; 1; 2; 3|])
+
+        //// wb
+        //let wr2 = client.WriteBytes("M", [|0; 1; 2; 3|], [|1uy; 0uy; 55uy; 0uy|])
+        //let bytes:byte[] = client.ReadBytes("M", [|0; 1; 2; 3|])
+        //let words:uint16[] = client.ReadUInt16s("M", [|0; 1; 2; 3|])
+
+        //// wx
+        //let wr3 = client.WriteBits("M", [|0; 7|], [|true; true|])
+        //let rr3 = client.ReadBytes("M", [|0|])
+
+        // ---- third party ----
+        // wb
+        let wr2 = client.WriteBytes("p/o", [|0; 1; 2; 3|], [|99uy; 98uy; 97uy; 96uy|])
+        match client.ReadBytes("p/o", [|0; 1; 2; 3|]) with
+        | Ok bytes ->
+            ()
+        | _ ->
+            failwith "ERROR"
+
+     
+        let mutable key = ""
+        while ( key <> null && not cts.IsCancellationRequested ) do
+            key <- Console.ReadLine()
+            if key <> null then
+                match key with
+                | "q" | "Q" -> key <- null
+                | "" -> ()
+                | _ ->
+                    match client.SendRequest(key) with
+                    | Ok value ->
+                        Console.WriteLine($"OK: {value}")
+                    | Error err ->
+                        Console.WriteLine($"ERR: {err}")
+
+                ()
+
+        while(not server.IsTerminated) do
+            logDebug("Waiting server terminated...")
+            Thread.Sleep(1000)
+        
+        logDebug("Exiting...")
+        Thread.Sleep(1000)
+        0

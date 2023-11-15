30c8c1937c53be9484347703cc82874d93d2cf6e
승훈 안 <ahn@dualsoft.com>
2023-11-15 21:29:19 +0900

    코드정리

--- DsDotNet/src/Engine/Engine.Import.Office/ImportPPT.fs
+++ DsDotNet/src/Engine/Engine.Import.Office/ImportPPT.fs
@@ -237,20 +237,42 @@ module ImportPPTModule =
     [<Extension>]
     type ImportPPT =
 
+    
         [<Extension>]
-        static member GetModel(path: string ) =
-            pptRepo.Clear()
-            loadingfromPPTs (path) |> fun (model,  pptRepo) -> model
-
-        [<Extension>]
-        static member GetLoadingAllSystem(path: string ) =
+        static member GetDSFromPPTWithLib(fullName: string) =
             pptRepo.Clear()
 
-            loadingfromPPTs (path)
-            |> fun (model,  pptRepo) ->
-                model,
-                pptRepo
-                |> Seq.map (fun f ->
-                    { System = f.Key
-                      Views = f.Key.GetViewNodes()
-                      })
+            do
+                // library 파일을 먼저 로딩해서 DS 파일로 변환한다.
+
+                let textLibPptx = $"{TextLibrary}.pptx" |> DsFile
+                // 파일 loadingfromPPTs 시 DS_Library.ds 만드는 용도
+                let libSys =
+                    let libFilePath =
+                        let runDir: DsPath =
+                            System.Reflection.Assembly.GetEntryAssembly().Location |> getFolderOfPath
+
+                        PathManager.getFullPath textLibPptx runDir
+
+                    let libModel: Model = loadingfromPPTs  libFilePath  |> Tuple.first
+                    libModel.System
+
+                let loadedlibFilePath =
+                    PathManager.getFullPath textLibPptx (PathManager.getFolderOfPath (fullName))
+
+                libSys.pptxToExportDS loadedlibFilePath |> ignore
+
+
+
+            let exportPath =
+                let sys =
+                    let model: Model = loadingfromPPTs  fullName  |> Tuple.first
+                    model.System
+
+                sys.pptxToExportDS fullName
+
+            let system, loadingPaths = ParserLoader.LoadFromActivePath exportPath
+
+            { System = system
+              ActivePaths = [ exportPath ]
+              LoadingPaths = loadingPaths }
--- DsDotNet/src/UnitTest/UnitTest.Engine/ImportOffice/pptTest.fs
+++ DsDotNet/src/UnitTest/UnitTest.Engine/ImportOffice/pptTest.fs
@@ -14,7 +14,7 @@ open Engine.Import.Office
 module pptTestModule =
 
     ///ppt로 부터 만든 모델을 text로 다시 읽어 이중 확인
-    let check (model:Model) =
+    let check (model:DSFromPPT) =
         let systemRepo = ShareableSystemRepository()
         let dsText =  model.System.ToDsText(true)
         let libdir = @$"{__SOURCE_DIRECTORY__}/../../UnitTest.Model/ImportOfficeExample/ppt/"
@@ -26,14 +26,14 @@ module pptTestModule =
     type PPTTest() =
         inherit EngineTestBaseClass()
 
-        [<Test>] member __.``System  test``    () = check (ImportPPT.GetModel $"{testpptPath}/T1_System.pptx")
-        [<Test>] member __.``Flow  test``      () = check (ImportPPT.GetModel $"{testpptPath}/T2_Flow.pptx")
-        [<Test>] member __.``Real  test``      () = check (ImportPPT.GetModel $"{testpptPath}/T3_Real.pptx")
-        [<Test>] member __.``Api  test``       () = check (ImportPPT.GetModel $"{testpptPath}/T4_Api.pptx")
-        [<Test>] member __.``Calltest``        () = check (ImportPPT.GetModel $"{testpptPath}/T5_Call.pptx")
-        [<Test>] member __.``Alias  test``     () = check (ImportPPT.GetModel $"{testpptPath}/T6_Alias.pptx")
-        [<Test>] member __.``CopySystem test`` () = check (ImportPPT.GetModel $"{testpptPath}/T7_CopySystem.pptx")
-        [<Test>] member __.``Safety test``     () = check (ImportPPT.GetModel $"{testpptPath}/T8_Safety.pptx")
-        [<Test>] member __.``Group test``      () = check (ImportPPT.GetModel $"{testpptPath}/T9_Group.pptx")
-        [<Test>] member __.``Button test``     () = check (ImportPPT.GetModel $"{testpptPath}/T10_Button.pptx")
-        [<Test>] member __.``SubLoading test`` () = check (ImportPPT.GetModel $"{testpptPath}/T11_SubLoading.pptx")
+        [<Test>] member __.``System  test``    () = check (ImportPPT.GetDSFromPPTWithLib $"{testpptPath}/T1_System.pptx")
+        [<Test>] member __.``Flow  test``      () = check (ImportPPT.GetDSFromPPTWithLib $"{testpptPath}/T2_Flow.pptx")
+        [<Test>] member __.``Real  test``      () = check (ImportPPT.GetDSFromPPTWithLib $"{testpptPath}/T3_Real.pptx")
+        [<Test>] member __.``Api  test``       () = check (ImportPPT.GetDSFromPPTWithLib $"{testpptPath}/T4_Api.pptx")
+        [<Test>] member __.``Calltest``        () = check (ImportPPT.GetDSFromPPTWithLib $"{testpptPath}/T5_Call.pptx")
+        [<Test>] member __.``Alias  test``     () = check (ImportPPT.GetDSFromPPTWithLib $"{testpptPath}/T6_Alias.pptx")
+        [<Test>] member __.``CopySystem test`` () = check (ImportPPT.GetDSFromPPTWithLib $"{testpptPath}/T7_CopySystem.pptx")
+        [<Test>] member __.``Safety test``     () = check (ImportPPT.GetDSFromPPTWithLib $"{testpptPath}/T8_Safety.pptx")
+        [<Test>] member __.``Group test``      () = check (ImportPPT.GetDSFromPPTWithLib $"{testpptPath}/T9_Group.pptx")
+        [<Test>] member __.``Button test``     () = check (ImportPPT.GetDSFromPPTWithLib $"{testpptPath}/T10_Button.pptx")
+        [<Test>] member __.``SubLoading test`` () = check (ImportPPT.GetDSFromPPTWithLib $"{testpptPath}/T11_SubLoading.pptx")

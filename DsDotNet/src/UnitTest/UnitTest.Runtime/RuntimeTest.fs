2a2fd2f987987aa9b93503f3a197195884290f62
승훈 안 <ahn@dualsoft.com>
2023-11-27 18:36:41 +0900

    Runtime Create Test path 정규화

--- DsDotNet/src/UnitTest/UnitTest.Runtime/RuntimeTest.fs
+++ DsDotNet/src/UnitTest/UnitTest.Runtime/RuntimeTest.fs
@@ -13,7 +13,7 @@ module RuntimeTest =
     
     [<Fact>]
     let ``Runtime Create Test`` () = 
-        let testPPT = @$"{__SOURCE_DIRECTORY__}../../../UnitTest/UnitTest.Model/ImportOfficeExample/exportDS/testA/testMy/my.pptx"
+        let testPPT = Path.GetFullPath @$"{__SOURCE_DIRECTORY__}../../../UnitTest/UnitTest.Model/ImportOfficeExample/exportDS/testA/testMy/my.pptx"
         RuntimeDS.IP  <- "192.168.9.100"
 
         let zipPath = ImportPPT.GetRuntimeZipFromPPT testPPT

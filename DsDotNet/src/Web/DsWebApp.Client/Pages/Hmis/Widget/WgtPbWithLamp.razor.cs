7f68146adcbda081aa7ff2338942a9bb9b8e77c2
kwak <kwak@dualsoft.com>
2023-12-04 15:33:01 +0900

    - [Web] Rename: CompWgt* -> Wgt*

--- DsDotNet/src/Web/DsWebApp.Client/Pages/Hmis/Widget/WgtPbWithLamp.razor.cs
+++ DsDotNet/src/Web/DsWebApp.Client/Pages/Hmis/Widget/WgtPbWithLamp.razor.cs
@@ -3,7 +3,7 @@ namespace DsWebApp.Client.Pages.Hmis.Widget
     /// <summary>
     /// Component Widget Push Button With Lamp
     /// </summary>
-    public partial class CompWgtPbWithLamp
+    public partial class WgtPbWithLamp
     {
     }
 }

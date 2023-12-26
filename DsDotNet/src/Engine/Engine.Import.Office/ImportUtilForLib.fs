5907efb9c5c66f63c4f76baa8efde831f24936b4
ahn <ahn@dualsoft.com>
2023-12-26 13:03:02 +0900

    layout 정의 ppt UI

--- DsDotNet/src/Engine/Engine.Import.Office/ImportUtilForLib.fs
+++ DsDotNet/src/Engine/Engine.Import.Office/ImportUtilForLib.fs
@@ -74,21 +74,22 @@ module ImportUtilForLib =
         if not (devOrg.ApiItems.any (fun f -> f.Name = apiPureName)) then
             node.Shape.ErrorName(ErrID._49, node.PageNum)
 
-        let tasks = HashSet<DsTask>()
-        let jobType = getJobActionType apiName
-        match jobType with
-        | MultiAction cnt ->  
-            for i in [1..cnt] do
-                let devOrg = if i = 1 then devOrg
-                             else ParserLoader.LoadFromActivePath libFilePath |> fst
 
-                let mutiName = $"{loadedName}_G{i}"
-                tasks.Add(getLoadedTasks devOrg mutiName)|>ignore
-        | _->
-            tasks.Add(getLoadedTasks devOrg loadedName)|>ignore
+        let job =
+            let tasks = HashSet<DsTask>()
+            match getJobActionType apiName with
+            | MultiAction cnt ->  
+                for i in [1..cnt] do
+                    let devOrg = if i = 1 then devOrg
+                                    else ParserLoader.LoadFromActivePath libFilePath |> fst
+
+                    let mutiName = $"{loadedName}_G{i}"
+                    tasks.Add(getLoadedTasks devOrg mutiName)|>ignore
+            | _->
+                tasks.Add(getLoadedTasks devOrg loadedName)|>ignore
+            Job(loadedName + "_" + apiName, tasks |> Seq.toList)
 
 
-        let job = Job(loadedName + "_" + apiName, tasks |> Seq.toList)
         mySys.Jobs.Add(job)
 
         let call = Call.Create(job, parent)

7a2a2340161b1b4c9d6f855d469dfa1733f0f513
kwak <kwak@dualsoft.com>
2024-01-01 14:23:16 +0900

    - [Info] 초기 model 로딩 작업 중.

--- DsDotNet/src/Web/DsWebApp.Client/Components/DxxGrid.cs
+++ DsDotNet/src/Web/DsWebApp.Client/Components/DxxGrid.cs
@@ -1,21 +1,21 @@
-using Blazored.LocalStorage;
+//using Blazored.LocalStorage;
 
-using Microsoft.AspNetCore.Components;
+//using Microsoft.AspNetCore.Components;
 
-namespace DsWebApp.Client.Components;
+//namespace DsWebApp.Client.Components;
 
-public class DxxGrid : DxxGridBase
-{
-    [Inject] ILocalStorageService LocalStorage { get; set; }
-    protected override async Task OnInitializedAsync()
-    {
-        // base.OnInitializedAsync 이전에 수행.
-        base.ClientSettings = await DsClientSettings.ReadAsync(LocalStorage);
+//public class DxxGrid : DxxGridBase
+//{
+//    [Inject] ILocalStorageService LocalStorage { get; set; }
+//    protected override async Task OnInitializedAsync()
+//    {
+//        // base.OnInitializedAsync 이전에 수행.
+//        base.ClientSettings = await DsClientSettings.ReadAsync(LocalStorage);
 
-        await base.OnInitializedAsync();
-    }
-    //protected override async Task<ClientSettings> GetClientSettings()
-    //{
-    //    return (ClientSettings)(await DsClientSettings.ReadAsync(LocalStorage));
-    //}
-}
+//        await base.OnInitializedAsync();
+//    }
+//    //protected override async Task<ClientSettings> GetClientSettings()
+//    //{
+//    //    return (ClientSettings)(await DsClientSettings.ReadAsync(LocalStorage));
+//    //}
+//}

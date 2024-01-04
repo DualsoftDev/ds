bd497e58055660e1a701a085819a94f0ddd1c1cb
ds <dskim9752@gmail.com>
2024-01-04 09:43:42 +0900

    ds ppt build v0.9.2.22

--- DsDotNet/src/Engine/Engine.Common.Assembly.fs
+++ DsDotNet/src/Engine/Engine.Common.Assembly.fs
@@ -15,7 +15,7 @@ open System.Reflection
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyTitle("Dualsoft Engine packages")>]
 [<assembly: AssemblyProduct("DS Engine")>]
-[<assembly: AssemblyVersion($"0.9.2.21")>]
+[<assembly: AssemblyVersion($"0.9.2.22")>]
 
 
 do ()

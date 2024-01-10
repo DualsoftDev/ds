fdf537e9f60a6e19d6ceaad786a4145d06872522
kwak <kwak@dualsoft.com>
2024-01-10 13:35:41 +0900

    - Model Change, Cpu Running State change 이벤트 작업 중.

--- DsDotNet/src/Web/DsWebApp.Client/ClientGlobal.cs
+++ DsDotNet/src/Web/DsWebApp.Client/ClientGlobal.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNetCore.SignalR.Client;
 
 using System.ComponentModel;
 using System.Net.Http.Json;
+using System.Reactive.Disposables;
 using System.Reactive.Subjects;
 
 using static Engine.Core.HmiPackageModule;
@@ -22,6 +23,8 @@ namespace DsWebApp.Client;
 
 public class ClientGlobal : ClientGlobalBase, INotifyPropertyChanged
 {
+    CompositeDisposable _disposables = new();
+    HubConnection _hubConnectionModel;
     public HMIPackage HmiPackage { get; set; }
 
     public Subject<TagWeb> TagChangedSubject = new Subject<TagWeb>();
@@ -32,53 +35,93 @@ public class ClientGlobal : ClientGlobalBase, INotifyPropertyChanged
     public bool IsCpuRunning { get; set; }
     public event PropertyChangedEventHandler PropertyChanged;
 
-    public async Task InitializeAsync(HttpClient http, ILocalStorageService localStorage)
+    public async Task InitializeAsync(HttpClient http, NavigationManager navigationManager, ILocalStorageService localStorage)
     {
         if (ServerSettings == null)
+        {
             ServerSettings = await http.GetFromJsonAsync<ServerSettings>("api/serversettings");
+            ModelDsZipPath = ServerSettings.RuntimeModelDsZipPath;
+        }
 
         if (ServerSettings == null)
             Console.Error.WriteLine("Error: ServerSettings is null.");
 
         base.ClientSettings = await DsClientSettings.ReadAsync(localStorage);
-    }
 
-    RuntimeModelDto _modelDto;
-    HubConnection _hubConnectionModel;
-    public async Task<ResultSerializable<RuntimeModelDto, ErrorMessage>> GetModelDtoAsync(HttpClient http)
-    {
-        if (_modelDto == null)
-        {
-            var result = await http.GetResultSimpleAsync<RuntimeModelDto>($"api/model");
-            result.Iter(
-                ok => _modelDto = ok,
-                err => Console.Error.WriteLine($"Error: {err}"));
+        var result = await http.GetResultSimpleAsync<RuntimeModelDto>($"api/model");
+        result.Iter(
+            ok =>
+            {
+                ModelDsZipPath = ok.SourceDsZipPath;
+                IsCpuRunning = ok.IsCpuRunning;
+            },
+            err => Console.Error.WriteLine($"Error: {err}"));
 
-            return result;
-        }
 
-        return ResultSerializable<RuntimeModelDto, ErrorMessage>.Ok(_modelDto);
-    }
-    public async Task<IDisposable> MonitorModelChangeAsync(NavigationManager navigationManager, HttpClient http, Action<RuntimeModelDto> onModelChanged)
-    {
-        if (_hubConnectionModel == null)
-            _hubConnectionModel = await navigationManager.ToAbsoluteUri("/hub/model").StartHubAsync();
+        _hubConnectionModel = await navigationManager.ToAbsoluteUri("/hub/model").StartHubAsync();
 
         IDisposable subscription =
-            _hubConnectionModel.On<RuntimeModelDto>(SK.S2CNModelChanged, async (RuntimeModelDto newModel) =>
+            _hubConnectionModel.On<string>(SK.S2CNModelChanged, async modelDsZipPath =>
             {
-                Console.WriteLine($"Model change detected on signalR: {newModel.SourceDsZipPath}, {newModel.IsCpuRunning}");
-
+                Console.WriteLine($"Model change detected on signalR: {modelDsZipPath}");
+                ModelDsZipPath = modelDsZipPath;
                 var result = await http.GetResultSimpleAsync<HMIPackage>($"api/hmi/package");
                 result.Iter(
                     ok => HmiPackage = ok.Tee(pkg => pkg.BuildTagMap()),
                     err => Console.Error.WriteLine(err));
-
-                _modelDto = newModel;
-                onModelChanged(newModel);   // e.g StateHasChanged();
             });
-        return subscription;
+        _disposables.Add(subscription);
+
+        subscription =
+            _hubConnectionModel.On<bool>(SK.S2CNCpuRunningStatusChanged, (bool isCpuRunning) =>
+            {
+                Console.WriteLine($"-------------- CPU RUNNING STATE CHANGED TO: {isCpuRunning}");
+                IsCpuRunning = isCpuRunning;
+            });
+        _disposables.Add(subscription);
     }
+
+    //RuntimeModelDto _modelDto;
+    //HubConnection _hubConnectionModel;
+    //public async Task<ResultSerializable<RuntimeModelDto, ErrorMessage>> GetModelDtoAsync(HttpClient http)
+    //{
+    //    if (_modelDto == null)
+    //    {
+    //        var result = await http.GetResultSimpleAsync<RuntimeModelDto>($"api/model");
+    //        result.Iter(
+    //            ok =>
+    //            {
+    //                _modelDto = ok;
+    //                IsCpuRunning = _modelDto.IsCpuRunning;
+    //            },
+    //            err => Console.Error.WriteLine($"Error: {err}"));
+
+    //        return result;
+    //    }
+
+    //    return ResultSerializable<RuntimeModelDto, ErrorMessage>.Ok(_modelDto);
+    //}
+    //public async Task<IDisposable> MonitorModelChangeAsync(NavigationManager navigationManager, HttpClient http, Action<RuntimeModelDto> onModelChanged)
+    //{
+    //    if (_hubConnectionModel == null)
+    //        _hubConnectionModel = await navigationManager.ToAbsoluteUri("/hub/model").StartHubAsync();
+
+    //    IDisposable subscription =
+    //        _hubConnectionModel.On<string>(SK.S2CNModelChanged, async (modelDsZipPath) =>
+    //        {
+    //            ModelDsZipPath = modelDsZipPath;
+    //            Console.WriteLine($"Model change detected on signalR: {modelDsZipPath}, {newModel.IsCpuRunning}");
+
+    //            var result = await http.GetResultSimpleAsync<HMIPackage>($"api/hmi/package");
+    //            result.Iter(
+    //                ok => HmiPackage = ok.Tee(pkg => pkg.BuildTagMap()),
+    //                err => Console.Error.WriteLine(err));
+
+    //            _modelDto = newModel;
+    //            onModelChanged(newModel);   // e.g StateHasChanged();
+    //        });
+    //    return subscription;
+    //}
 
 
     static int _counter { get; set; } = 0;

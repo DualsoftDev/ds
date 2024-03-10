f9ca48c3c3d8eaa38e7640cc01e48142e16a064a
승훈 안 <ahn@dualsoft.com>
2024-03-11 08:02:45 +0900

    설치시 덮어쓰기

--- DsDotNet/src/Engine/Engine.Common.Assembly.fs
+++ DsDotNet/src/Engine/Engine.Common.Assembly.fs
@@ -15,7 +15,7 @@ open System.Reflection
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyTitle("Dualsoft Engine packages")>]
 [<assembly: AssemblyProduct("DS Engine")>]
-[<assembly: AssemblyVersion($"0.9.4.0")>]
+[<assembly: AssemblyVersion($"0.9.4.4")>]
 
 
 do ()

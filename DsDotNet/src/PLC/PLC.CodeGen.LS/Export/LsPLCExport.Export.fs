ca2aa4f2db541231ba5cebc62110baf815d1e45d
kwak <kwak@dualsoft.com>
2024-04-19 14:38:38 +0900

    - LS XGK PLC export 작업 중 : Timer resolution 적용

--- DsDotNet/src/PLC/PLC.CodeGen.LS/Export/LsPLCExport.Export.fs
+++ DsDotNet/src/PLC/PLC.CodeGen.LS/Export/LsPLCExport.Export.fs
@@ -239,22 +239,25 @@ module XgiExportModule =
 
     type XgxProjectParamsProperties with
         /// Project XML 문서로부터 필요한 정보를 추출
-        member x.FillPropertiesFromXmlDocument(xdoc:XmlDocument) =
-            let dic = collectXgkBasicParameters xdoc
-            let readRange prefix =
-                let s = dic[$"{prefix}_START"]
-                let e = dic[$"{prefix}_END"]
-                (s, e)
+        member x.FillPropertiesFromXmlDocument(prjParam: XgxProjectParams, xdoc:XmlDocument) =
+            match prjParam.TargetType with
+            | XGK ->
+                let dic = collectXgkBasicParameters xdoc
+                let readRange prefix =
+                    let s = dic[$"{prefix}_START"]
+                    let e = dic[$"{prefix}_END"]
+                    (s, e)
 
-            [|
-                0.1, "T_100US_AREA_RANGE"
-                1.0, "T_001MS_AREA_RANGE"
-                10.0, "T_010MS_AREA_RANGE"
-                100.0, "T_100MS_AREA_RANGE"
-            |] |> map (fun (res, prefix) -> res, readRange prefix)
-               |> map XgkTimerResolutionSpec
-               |> x.XgxTimerResolutionSpec.AddRange
-            ()
+                [|
+                    0.1, "T_100US_AREA_RANGE"
+                    1.0, "T_001MS_AREA_RANGE"
+                    10.0, "T_010MS_AREA_RANGE"
+                    100.0, "T_100MS_AREA_RANGE"
+                |] |> map (fun (res, prefix) -> res, readRange prefix)
+                   |> map XgkTimerResolutionSpec
+                   |> x.XgxTimerResolutionSpec.AddRange
+                ()            | _ -> ()
+
 
     type XgxPOUParams with
 
@@ -366,7 +369,7 @@ module XgiExportModule =
                     let doc = DualXmlDocument.loadFromFile existing
                     doc, prjParam
 
-            prjParam.Properties.FillPropertiesFromXmlDocument(xdoc)
+            prjParam.Properties.FillPropertiesFromXmlDocument(prjParam, xdoc)
 
             let { ProjectName = projName
                   TargetType = targetType

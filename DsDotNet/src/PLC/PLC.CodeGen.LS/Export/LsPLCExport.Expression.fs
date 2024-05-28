05a9430370033283bbd1469d8d8dfee4e9aefd2c
kwak <kwak@dualsoft.com>
2024-05-29 07:22:26 +0900

    - Counter/Timer Complex condition 수정 관련 코드 정리 중.

--- DsDotNet/src/PLC/PLC.CodeGen.LS/Export/LsPLCExport.Expression.fs
+++ DsDotNet/src/PLC/PLC.CodeGen.LS/Export/LsPLCExport.Expression.fs
@@ -18,6 +18,7 @@ module LsPLCExportExpressionModule =
         member val Statements = statements    // ResizeArray<Statement>
         member val ExpressionStore:IStorage option = None with get, set
 
+
     type IExpression with
         /// 주어진 Expression 을 multi-line text 형태로 변환한다.
         member exp.ToTextFormat() : string =
@@ -98,3 +99,6 @@ module LsPLCExportExpressionModule =
                     newFn
                 | _ -> failwith "Invalid expression"
             traverse 0 exp resultStore
+
+
+

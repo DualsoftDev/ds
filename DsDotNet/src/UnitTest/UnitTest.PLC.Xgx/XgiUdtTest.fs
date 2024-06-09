8d5b6963fd6c0973875e1fbb4a385999e1b5137f
kwak <kwak@dualsoft.com>
2024-06-10 07:01:56 +0900

    - UDT: Parsing..

--- DsDotNet/src/UnitTest/UnitTest.PLC.Xgx/XgiUdtTest.fs
+++ DsDotNet/src/UnitTest/UnitTest.PLC.Xgx/XgiUdtTest.fs
@@ -16,10 +16,7 @@ module XgiUdtTestModule =
     type XgiUdtTest() =
         inherit XgxTestBaseClass(XGI)
 
-        [<Test>]
-        member x.``xgi udt decl test`` () =
-            let storages = Storages()
-            let code = """
+        let udtBaseCode = """
     struct Person {
         string name;
         int age;
@@ -27,6 +24,24 @@ module XgiUdtTestModule =
     Person hong;
     Person people[10];
     """
+
+        [<Test>]
+        member x.``xgi udt decl test`` () =
+            let storages = Storages()
+            let statements = parseCodeForWindows storages udtBaseCode
+            let f = getFuncName()
+            let xml = x.generateXmlForTest f storages (map withNoComment statements)
+            x.saveTestResult f xml
+
+        [<Test>]
+        member x.``xgi udt member assign test`` () =
+            let storages = Storages()
+            let code = udtBaseCode + """
+            $hong.name = "Hong";
+            $hong.age = 20;
+            $people[0].name = "Kim";
+            $people[0].age = 30;
+            """
 
             let statements = parseCodeForWindows storages code
             let f = getFuncName()

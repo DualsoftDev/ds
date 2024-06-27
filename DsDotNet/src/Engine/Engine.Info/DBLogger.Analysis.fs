1e8efe44508e4c9995ae138dcfd921fb08a9b129
kwak <kwak@dualsoft.com>
2024-06-27 09:16:37 +0900

    - Logging 분석 작업 중.

--- DsDotNet/src/Engine/Engine.Info/DBLogger.Analysis.fs
+++ DsDotNet/src/Engine/Engine.Info/DBLogger.Analysis.fs
@@ -1,34 +1,19 @@
 namespace Engine.Info
 
-open System
-open System.Configuration
-open System.Collections.Concurrent
-open Dapper
 open Engine.Core
-open Microsoft.Data.Sqlite
 open Dual.Common.Core.FS
-open Dual.Common.Db
-open System.Collections.Generic
-open System.Data
-open System.Reactive.Disposables
-open System.Threading.Tasks
 open DBLoggerORM
 
 [<AutoOpen>]
 module internal DBLoggerAnalysisModule =
-    let groupDurationsByFqdn (logs: ORMVwLog list) (fqdn:string) =
+    let private groupDurationsByFqdn_FoldBackImpl(logs: ORMVwLog list) (fqdn:string) : ORMVwLog list list =
         let mutable started = false
         let mutable building = []
-        let mutable counter = 0
         let folder (l:ORMVwLog) acc =
-            counter <- counter + 1
-            if (l.TagKind.IsOneOf(int VertexTag.going, int VertexTag.finish)) then
-                ()
             let isOn = unbox l.Value = 1L
             if l.Fqdn = fqdn && isOn then
                 if started then
-                    //if l.TagKind = int VertexTag.finish then
-                    if l.TagKind = int VertexTag.homing then
+                    if l.TagKind = int VertexTag.finish then
                         started <- false
                 else
                     if l.TagKind = int VertexTag.going then
@@ -44,22 +29,37 @@ module internal DBLoggerAnalysisModule =
                 res
 
         let result = List.foldBack folder logs []
-        tracefn $"LOOP: {counter}"
         result
 
 
-        //for l in logs do
-        //    if l.Fqdn = fqdn then
-        //        if l.TagKind = VertexTag.going && l.Value = 1 then
-        //            started <- true
-        //        elif l.TagKind = VertexTag.coming && l.Value = 1 && started then
-        //        yield l
-        //    else
-        //        yield! Array.empty<ORMVwLog> // yield! Array.empty<ORMVwLog> is equivalent to yield! []
-        //let spitLogs logs =
-        //logs
-        //|> Seq.groupBy (fun log -> log.LogTime.Date)
-        //|> Seq.map (fun (date, logs) -> date, logs |> Seq.length)
-        //|> Seq.toArray
+    /// log 를 fqdn 별로 그룹핑하여 반환한다.
+    /// - log 는 시간순 정렬되어 있어야 한다.
+    /// - fqdn 의 going ON 부터 finish ON 까지의 log 를 그룹핑하여 반환한다.
+    let groupDurationsByFqdn (logs: ORMVwLog list) (fqdn:string) : ORMVwLog list list =
+        let mutable started = false
+        let mutable building = []
+        let mutable counter = 0
+        let folder acc (l:ORMVwLog) =
+            counter <- counter + 1
+            if (l.TagKind.IsOneOf(int VertexTag.going, int VertexTag.finish)) then
+                ()
+            let isOn = l.Value.ToString() = "1"
+            if l.Fqdn = fqdn && isOn then
+                if started then
+                    //if l.TagKind = int VertexTag.finish then
+                    if l.TagKind = int VertexTag.homing then
+                        started <- false
+                else
+                    if l.TagKind = int VertexTag.going then
+                        started <- true
+            if started then
+                building <- l::building
+                acc
+            else
+                let res = if building.IsEmpty then acc else (building |> List.rev)::acc
+                building <- []
+                res
 
-
+        let result = List.fold folder [] logs |> List.rev
+        tracefn $"LOOP: {counter}"
+        result

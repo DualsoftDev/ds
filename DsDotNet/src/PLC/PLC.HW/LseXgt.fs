1d14e8968737bc6921e30ccc9b25384f1bcae03c
kwak <kwak@dualsoft.com>
2024-10-04 17:23:28 +0900

    - PptToDS: Transpiling..

--- DsDotNet/src/PLC/PLC.HW/LseXgt.fs
+++ DsDotNet/src/PLC/PLC.HW/LseXgt.fs
@@ -4,6 +4,7 @@ open Newtonsoft.Json
 open Dual.Common.Core.FS
 open Dual.Common.Base.FS
 open System.Runtime.CompilerServices
+open System.ComponentModel
 
 type PLCType =
     | Xgi
@@ -32,8 +33,7 @@ type Platform =
 /// LSE XGT 기종
 [<AutoOpen>]
 module rec XGT =
-
-
+    let [<Literal>] MaxNumberBases = 8
     let [<Literal>] MaxNumberSlots = 12
     /// One of | 8 | 16 | 32 | 64
     type PlcIoSlotCapacity =
@@ -79,16 +79,20 @@ module rec XGT =
         let slots = ResizeArray(slots)
         do
             assert(slots.Count <= MaxNumberSlots)
-        //new() = Base(repeatWith (fun () -> IoSlot()) |> take MaxNumberSlots |> toArray)
+
         new() = Base([])
         member val Slots = slots with get, set     // get, set for newtonsoft
-        static member Create(isFixedSlotAllocation) =
+        static member Create() =
             Base([ for i in 0..MaxNumberSlots-1 -> IoSlot() ])
 
         [<JsonIgnore>]
+        [<DisplayName("총 슬롯수")>]
         member x.NumSlot
             with get() = x.Slots.Count
             and set(n) = x.Slots.Resize(n)
+        [<JsonIgnore>]
+        [<DisplayName("사용 슬롯수")>]
+        member x.NumUsedSlot = x.Slots |> Seq.filter(fun s -> not s.IsEmpty) |> Seq.length
         member x.GetCapacity(isFixedSlotAllocation:bool) =
             if isFixedSlotAllocation then
                 64 * 16
@@ -115,29 +119,37 @@ module rec XGT =
         static member Create(plcType, isFixedSlotAllocation) =
             PlcHw(plcType, isFixedSlotAllocation)
             |> tee(fun plc ->
-                plc.Bases <- [| for i in 0..8 -> Base.Create(isFixedSlotAllocation) |]
+                plc.Bases <- [| for i in 1..MaxNumberBases -> Base.Create() |]
             )
 
     /// PLC IO slot 구성에 따라서 가용한 io bit 번호를 뱉는 함수.  더 이상 가용 bit 가 없으면 None 반환
-    type IOAllocatorFunction = unit -> int option
+    type IOAllocatorFunction = unit -> string option
 
     type PlcHw with
-        member x.CreateIOHaystacks(): int[] * int[] =
+        member x.CreateIOHaystacks(): string[] * string[] =
+            let createAddress(isInput:bool, bse:int, slot:int, bitOffset:int, totalSlotOffset:int) =
+                match x.PLCType, isInput with
+                | Xgi, true -> $"%%IX{bse}.{slot}.{bitOffset}"
+                | Xgi, false -> $"%%QX{bse}.{slot}.{bitOffset}"
+                | Xgk, _ ->
+                    let w = totalSlotOffset / 16
+                    let b = totalSlotOffset % 16
+                    sprintf "P%04d%X" w b
             let xss, yss =
                 seq {
 
                     let mutable baseStart = 0
                     // 이번 slot 의 시작 bit 주소
-                    for bbase in x.Bases do
+                    for b, bbase in x.Bases.Indexed() do
                         let mutable slotStart = baseStart
-                        for (i, slot) in bbase.Slots.Indexed() do
+                        for s, slot in bbase.Slots.Indexed() do
                             if not slot.IsEmpty then
                                 let cap= slot.Length
-
+                                let addresses = [| for i in 0..cap-1 -> createAddress(slot.IsInput, b, s, i, i+slotStart)|]
                                 if slot.IsInput then
-                                    yield [| slotStart .. slotStart+cap|], [||]
+                                    yield addresses, [||]
                                 else
-                                    yield [||], [| slotStart .. slotStart+cap |]
+                                    yield [||], addresses
 
                             slotStart <- slotStart + slot.GetCapacity(x.IsFixedSlotAllocation)
 
@@ -149,7 +161,7 @@ module rec XGT =
             xs, ys
 
 
-        member x.CreateIOAllocator(forbiddenXs:int seq, forbiddenYs:int seq) =
+        member x.CreateIOAllocator(forbiddenXs:string seq, forbiddenYs:string seq) =
             let xs, ys = x.CreateIOHaystacks()
 
             let availableXs = xs |> Seq.except forbiddenXs
@@ -158,3 +170,11 @@ module rec XGT =
             let outputAllocator:IOAllocatorFunction = Seq.tryEnumerate availableYs
             inputAllocator, outputAllocator
 
+type XGTDupExtensionForCSharp =
+    [<Extension>] static member Duplicate(slot:IoSlot) = IoSlot(slot.IsEmpty, slot.IsInput, slot.IsDigital, slot.Length)
+    [<Extension>] static member Duplicate(ioBase:Base) = Base(ioBase.Slots.Map(_.Duplicate()))
+    [<Extension>]
+    static member Duplicate(plcHw:PlcHw) =
+        let y = PlcHw.Create(plcHw.PLCType, plcHw.IsFixedSlotAllocation)
+        y.Bases <- plcHw.Bases.Map(_.Duplicate())
+        y

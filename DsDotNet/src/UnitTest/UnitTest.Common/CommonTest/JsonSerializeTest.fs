90bb5881c1d01642586edd71a16303e6fcefac8a
kwak <kwak@dualsoft.com>
2024-11-01 16:53:08 +0900

    - FSharp data JSON serialize library added.

--- DsDotNet/src/UnitTest/UnitTest.Common/CommonTest/JsonSerializeTest.fs
+++ DsDotNet/src/UnitTest/UnitTest.Common/CommonTest/JsonSerializeTest.fs
@@ -6,19 +6,41 @@ open Dual.Common.UnitTest.FS
 open NUnit.Framework
 open System
 open Newtonsoft.Json
+open FSharp.Json
+
+(*
+    ------- F# JSON serialization test -------
+    1. chiron: json computation expression 이용?
+     - https://www.codesuji.com/2019/01/07/Chiron-Introduction/
+     - https://github.com/xyncro/chiron
+    2. Newtonsoft.Json.FSharp.JsonConvert 사용
+    3. FSharp.Json 사용
+     - https://github.com/fsprojects/FSharp.Json
+*)
+
+
+type NewtonsoftJson = Newtonsoft.Json.JsonConvert
+//type NewtonsoftFSharp = Newtonsoft.Json.FSharp.JsonConvert
+type SystemTextJson = System.Text.Json.JsonSerializer
+//type FSharpJson = FSharp.Json.Json
+
+/// DB log table 의 row 항목
+type MyORMLog(id: int, storageId: int, at: DateTime, value: obj) =
+    new() = MyORMLog(-1, -1, DateTime.MaxValue, null)
+    member val Id = id with get, set
+    member val StorageId = storageId with get, set
+    member val At = at with get, set
+    member val Value = value with get, set
+
+type FSharpRecord = {
+    Name  : string
+    Value : obj
+    Message : string
+    Tuple: obj*string
+}
 
 [<AutoOpen>]
 module JsonSerializeTestModule =
-    type NewtonsoftJson = Newtonsoft.Json.JsonConvert
-
-    /// DB log table 의 row 항목
-    type MyORMLog(id: int, storageId: int, at: DateTime, value: obj) =
-        new() = MyORMLog(-1, -1, DateTime.MaxValue, null)
-        member val Id = id with get, set
-        member val StorageId = storageId with get, set
-        member val At = at with get, set
-        member val Value = value with get, set
-
     [<TestFixture>]
     type ObjectSerializeTest() =
 
@@ -27,6 +49,10 @@ module JsonSerializeTestModule =
         // 원래의 int, long, ulong => long type 으로 변환
         // float, double, decimal => double type 으로 변환
         let unsupportedObjects = [| box 8y; 8uy; 123s; 123us; 456; 457u; 1234567890; 1234567890UL; 1234567890.0f; 1234567890.0M; |]
+
+        let record = {Name="kwak"; Value=3.14; Message="Hello"; Tuple=(3, "three")}
+
+
         [<Test>]
         member _.``Newtonsoft Object SerializationTest1`` () =
             for o in supportedObjects do
@@ -54,3 +80,62 @@ module JsonSerializeTestModule =
                 log.Value === log2.Value
                 tracefn "%s" json
             ()
+
+        [<Test>]
+        member _.``NewtonSoft FSharpRecordSerializeTest``() =
+            let str = NewtonsoftJson.SerializeObject(record)
+            let xx = NewtonsoftJson.DeserializeObject<FSharpRecord>(str)
+            let str2 = NewtonsoftJson.SerializeObject(xx)
+            str === str2
+
+        [<Test>]
+        member _.``SystemText FSharpRecordSerializeTest``() =
+            let str = SystemTextJson.Serialize(record)
+            let xx = SystemTextJson.Deserialize<FSharpRecord>(str)
+            let str2 = SystemTextJson.Serialize(xx)
+            str === str2
+
+
+
+
+
+    type TheRecord = {
+        Value: string
+    }
+
+    type TheUnion =
+    | NoFieldCase
+    | OneFieldCase of string
+    | ManyFieldsCase of string*int
+    | RecordCase of TheRecord
+
+    type OtherRecord = {
+        Union: TheUnion
+    }
+
+
+    [<TestFixture>]
+    type FSharpJsonSerializeTest() =
+        let record = {Name="kwak"; Value=3.14; Message="Hello"; Tuple=(3, "three")}
+
+        // record type 은 serialize 안됨.
+        [<Test>]
+        member _.``FSharpJson FSharpRecordSerializeTest``() =
+            let str = FSharpJson.Serialize record
+            let xx:FSharpRecord = FSharpJson.Deserialize<FSharpRecord> str
+            let str2 = FSharpJson.Serialize xx
+            str === str2
+
+        // record type 은 serialize 안됨.
+        [<Test>]
+        member _.``FSharpJson Discriminated Union Test``() =
+            let dus = [
+                OneFieldCase "one"
+                ManyFieldsCase ("many", 3)
+                RecordCase {Value="record"}
+                NoFieldCase
+            ]
+            let str = FSharpJson.Serialize dus
+            let xx = FSharpJson.Deserialize<TheUnion[]> str
+            let str2 = FSharpJson.Serialize xx
+            str === str2

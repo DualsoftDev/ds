9c0a6c6468c07511db3c4fb8ff2d3e2e7ffde2a9
승훈 안 <ahn@dualsoft.com>
2025-02-28 09:54:47 +0900

    opc

--- DsDotNet/src/OPC/OPC.DSServer/DsOPCServer.fs
+++ DsDotNet/src/OPC/OPC.DSServer/DsOPCServer.fs
@@ -87,7 +87,16 @@ module DsOPCServerConfig =
         // Server Configuration
         let serverConfig = ServerConfiguration()
         let serverPort = ServerConfigModule.GetServerPort()    
-        serverConfig.BaseAddresses.Add($"opc.tcp://localhost:{serverPort}")
+        serverConfig.BaseAddresses.Add($"https://ahn:{serverPort}")
+        serverConfig.BaseAddresses.Add($"https://localhost:{serverPort}")
+        serverConfig.BaseAddresses.Add($"opc.tcp://localhost:{serverPort}/ds")
+        serverConfig.AlternateBaseAddresses.Add($"opc.tcp://ahn:{serverPort}")
+        serverConfig.AlternateBaseAddresses.Add($"opc.tcp://127.0.0.1:{serverPort}")
+        serverConfig.MinRequestThreadCount <- 5
+        serverConfig.MaxRequestThreadCount <- 100
+        serverConfig.MaxQueuedRequestCount <- 2000
+
+
         //serverConfig.BaseAddresses.Add("https://localhost:2747")
         //serverConfig.AlternateBaseAddresses.Add("opc.tcp://127.0.0.1:55555")
         //serverConfig.AlternateBaseAddresses.Add("https://127.0.0.1:55555")

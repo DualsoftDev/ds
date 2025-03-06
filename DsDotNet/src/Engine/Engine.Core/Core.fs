7508021952d961f712c78058381bd589a3a28a3e
승훈 안 <ahn@dualsoft.com>
2025-03-06 10:15:56 +0900

    motion 이름 짧게 Dev + Api + motionStart or motionEnd

--- DsDotNet/src/Engine/Engine.Core/Core.fs
+++ DsDotNet/src/Engine/Engine.Core/Core.fs
@@ -563,7 +563,7 @@ module rec CoreModule =
 
                         // Create a new Real
                         let newReal:Real = flow.CreateReal(realName)
-                        newReal.Motion <- Some($"genMotion_{apiName}")
+                        newReal.Motion <- Some($"{apiName}")
 
                         flow.Graph.Vertices.OfType<Real>().Iter(fun r->r.Finished <- false)  //기존 Real이 원위치 취소
                         newReal.Finished <- true    //마지막 Real이 원위치

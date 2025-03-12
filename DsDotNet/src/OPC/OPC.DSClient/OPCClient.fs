f11c228eb0d9aa92331a83025add2f7fe854fb4c
승훈 안 <ahn@dualsoft.com>
2025-03-12 10:05:20 +0900

    .

--- DsDotNet/src/OPC/OPC.DSClient/OPCClient.fs
+++ DsDotNet/src/OPC/OPC.DSClient/OPCClient.fs
@@ -53,8 +53,7 @@ module OPCClientModule =
         member this.CreateKeepAliveEventHandler() =
             KeepAliveEventHandler(fun _sender args ->
                     if not (ServiceResult.IsGood(args.Status)) then
-                        failwithf "KeepAlive failed: %s" (args.Status.ToLongString())    
-                        printfn "OPC UA session lost. Attempting reconnection..."
+                        printfn $"OPC UA session lost. Attempting reconnection...  {args.Status.ToLongString()}"
                         if not isReconnecting then
                             isReconnecting <- true
                             this.ReconnectSession()

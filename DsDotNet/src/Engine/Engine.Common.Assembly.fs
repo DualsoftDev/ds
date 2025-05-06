c71b85b0774ba19a81268467e111c32102f05985
승훈 안 <ahn@dualsoft.com>
2025-05-07 08:49:25 +0900

    0.9.11.22

--- DsDotNet/src/Engine/Engine.Common.Assembly.fs
+++ DsDotNet/src/Engine/Engine.Common.Assembly.fs
@@ -22,5 +22,5 @@ open System.Reflection
 ///DS Language Version (Language Parser 수정시만 변경 Ver : 1.0.0.1)
 [<assembly: AssemblyInformationalVersion($"1.0.0.1")>]
 ///DS Engine Version
-[<assembly: AssemblyFileVersion($"0.9.11.21")>]
+[<assembly: AssemblyFileVersion($"0.9.11.22")>]
 do ()
